e5f5df856ef590de4cc6fcb66597d9e32ec23aff
Saikrishna <saikrishna.edupuganti@intel.com>
2018-12-19 20:14:29 -0800

    Netlink to be notified about events on neighbors and routes
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

---
+++ netlink.py
@@ -0,0 +1,45 @@
+from pyroute2 import IPDB
+import pprint
+import sys
+import signal
+from enum import Enum
+
+# http://fos.tech/posts/pyroute2-linux-networking-made-easy/
+class NetlinkEvents(Enum):
+    # A new neighbor has appeared
+    RTM_NEWNEIGH = 'RTM_NEWNEIGH'
+    # We're no longer watching a certain neighbor
+    RTM_DELNEIGH = 'RTM_DELNEIGH'
+    # A new network interface has been created
+    RTM_NEWLINK = 'RTM_NEWLINK'
+    # A network interface has been deleted
+    RTM_DELLINK = 'RTM_DELLINK'
+    # An IP address has been added to a network interface
+    RTM_NEWADDR = 'RTM_NEWADDR'
+    # An IP address has been deleted off of a network interface
+    RTM_DELADDR = 'RTM_DELADDR'
+    # A route has been added to the routing table
+    RTM_NEWROUTE = 'RTM_NEWROUTE'
+    # A route has been removed from the routing table
+    RTM_DELROUTE = 'RTM_DELROUTE'
+
+ipdb = IPDB()
+pp = pprint.PrettyPrinter(indent=3)
+
+def new_event_callback(ipdb, netlink_message, action):
+    if 1: #action == NetlinkEvents.RTM_NEWADDR.name:
+        print action
+        pp.pprint(netlink_message)
+        print 100*'-'
+
+if __name__ == "__main__":
+
+    event_callback = ipdb.register_callback(new_event_callback)
+    def cleanup(*args):
+        ipdb.unregister_callback(event_callback)
+        sys.exit()
+
+    signal.signal(signal.SIGINT, cleanup)
+    signal.signal(signal.SIGTERM, cleanup)
+    signal.pause()
+
