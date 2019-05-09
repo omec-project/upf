b80459ebca680a6d8d51f8fe18342d0d0a426e8e
Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
2019-05-09 04:36:13 +0000

    Remove extra var BESSCTL

--- reload.sh
+++ reload.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-BESSCTL="/opt/bess/bessctl/bessctl"
-docker exec bess $BESSCTL daemon reset -- run file /conf/spgwu.bess
-docker exec bess $BESSCTL show pipeline | tee pipeline.txt
+docker exec bess ./bessctl daemon reset -- run file /conf/spgwu.bess
+docker exec bess ./bessctl show pipeline | tee pipeline.txt
