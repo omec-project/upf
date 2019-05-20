e2f469af5bcb3d9315f93301eef420ece8668130
Muhammad Asim Jamshed <muhammad.jamshed@intel.com>
2019-05-20 19:30:39 +0000

    Clean up script and remove magic numbers
    Signed-off-by: Muhammad Asim Jamshed <muhammad.jamshed@intel.com>

--- route_control.py
+++ route_control.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+BESSD_HOST = 'localhost'
+BESSD_PORT = '10514'
 # for retrieving route entries
 import netifaces
 import iptools
@@ -11,55 +13,46 @@ except ImportError:
     raise
 
 def route_control():
-        # Retrieve ip address and netmask prefix for s1u
-        s1u_addr_block = netifaces.ifaddresses('s1u')
-        s1u_addr = s1u_addr_block[netifaces.AF_INET][0]['addr']
-        #print(s1u_addr)
-        s1u_netmask = s1u_addr_block[netifaces.AF_INET][0]['netmask']
-        #print(s1u_netmask)
-        #print(iptools.ipv4.netmask2prefix(s1u_netmask))
-        s1u_subnet = iptools.ipv4.subnet2block(s1u_addr + '/' + s1u_netmask)
-        #print(s1u_subnet[0])
 
-        # Retrieve ip address and netmask prefix for sgi
-        sgi_addr_block = netifaces.ifaddresses('sgi')
-        sgi_addr = sgi_addr_block[netifaces.AF_INET][0]['addr']
-        #print(sgi_addr)
-        sgi_netmask = sgi_addr_block[netifaces.AF_INET][0]['netmask']
-        #print(sgi_netmask)
-        #print(iptools.ipv4.netmask2prefix(sgi_netmask))
-        sgi_subnet = iptools.ipv4.subnet2block(sgi_addr + '/' + sgi_netmask)
-        #print(sgi_subnet[0])
+    for i in netifaces.interfaces():
+        addr_block = netifaces.ifaddresses(i)
+        if netifaces.AF_INET in addr_block:
+            for j in range(len(addr_block[netifaces.AF_INET])):
+                addr = addr_block[netifaces.AF_INET][j]['addr']
+                print(addr)
+                netmask = addr_block[netifaces.AF_INET][j]['netmask']
+                print(iptools.ipv4.netmask2prefix(netmask))
+                subnet = iptools.ipv4.subnet2block(addr + '/' + netmask)
 
-        # Connect to BESS (assuming host=localhost, port=10514 (default))
-        bess = BESS()
-        bess.connect(grpc_url='localhost:10514')
+                # Connect to BESS (assuming host=localhost, port=10514 (default))
+                bess = BESS()
+                bess.connect(grpc_url=BESSD_HOST+':'+BESSD_PORT)
 
-        # Pause bessd to avoid race condition (and potential crashes)
-        bess.pause_all()
+                # Pause bessd to avoid race condition (and potential crashes)
+                bess.pause_all()
 
-        # Pass s1u routing entry to bessd's s1u_routes module
-        response = bess.run_module_command('s1u_routes',
-                                           'add',
-                                           'IPLookupCommandAddArg',
-                                           {'prefix': s1u_subnet[0],
-                                            'prefix_len': iptools.ipv4.netmask2prefix(s1u_netmask),
-                                            'gate': 0})
-        if response.error.code != 0:
-            print('Error inserting s1u_route')
+                # Pass s1u routing entry to bessd's s1u_routes module
+                response = bess.run_module_command('s1u_routes',
+                                                   'add',
+                                                   'IPLookupCommandAddArg',
+                                                   {'prefix': subnet[0],
+                                                    'prefix_len': iptools.ipv4.netmask2prefix(netmask),
+                                                    'gate': 0})
+                if response.error.code != 0:
+                    print('Error inserting s1u_route')
 
-        # Pass sgi routing entry to bessd's sgi_routes module   
-        response = bess.run_module_command('sgi_routes',
-                                           'add',
-                                           'IPLookupCommandAddArg',
-                                           {'prefix': sgi_subnet[0],
-                                            'prefix_len': iptools.ipv4.netmask2prefix(sgi_netmask),
-                                            'gate': 0})
-        if response.error.code != 0:
-            print('Error inserting sgi_route')
+                # Pass sgi routing entry to bessd's sgi_routes module
+                response = bess.run_module_command('sgi_routes',
+                                                   'add',
+                                                   'IPLookupCommandAddArg',
+                                                   {'prefix': subnet[0],
+                                                    'prefix_len': iptools.ipv4.netmask2prefix(netmask),
+                                                    'gate': 0})
+                if response.error.code != 0:
+                    print('Error inserting sgi_route')
 
-        # Now resume bessd operations
-        bess.resume_all()
+                # Now resume bessd operations
+                bess.resume_all()
 
 if __name__ == '__main__':
     route_control()
