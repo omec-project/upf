d3ba6342c143548e9cb2e557668623506415e6e5
Muhammad Asim Jamshed <muhammad.jamshed@intel.com>
2019-05-30 19:15:15 +0000

    Made setup.sh script config-friendly
    Signed-off-by: Muhammad Asim Jamshed <muhammad.jamshed@intel.com>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+S1U_NAME="s1u"
+SGI_NAME="sgi"
+S1U_IP=198.18.0.1
+SGI_IP=198.19.0.1
+S1U_MAC=3c:fd:fe:b4:41:90
+SGI_MAC=3c:fd:fe:b4:41:91
+IPMASK=30
+
 docker build -t krsna1729/spgwu .
 
 docker stop bess
@@ -13,14 +21,16 @@ docker run --name bess -itd --cap-add NET_ADMIN \
 krsna1729/spgwu
 
 docker exec bess bash -c "
-ip link add s1u type veth peer name s1u-vdev;
-ip link add sgi type veth peer name sgi-vdev;
-ip link set s1u up;
-ip link set s1u-vdev up;
-ip link set sgi up;
-ip link set sgi-vdev up;
-ip addr add 198.18.0.1/30 dev s1u;
-ip addr add 198.19.0.1/30 dev sgi;
+ip link add $S1U_NAME type veth peer name $S1U_NAME-vdev;
+ip link add $SGI_NAME type veth peer name $SGI_NAME-vdev;
+ip link set $S1U_NAME up;
+ip link set $S1U_NAME-vdev up;
+ip link set $SGI_NAME up;
+ip link set $SGI_NAME-vdev up;
+ip addr add $S1U_IP/$IPMASK dev $S1U_NAME;
+ip addr add $SGI_IP/$IPMASK dev $SGI_NAME;
+ip link set dev $S1U_NAME address $S1U_MAC
+ip link set dev $SGI_NAME address $SGI_MAC
 ip route;
 "
 
