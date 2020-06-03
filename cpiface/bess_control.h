e913d6b359201698c99a52a78becc81710afed0e
Muhammad Asim Jamshed <muhammad.jamshed@intel.com>
2020-06-03 00:19:06 -0700

    Reduced number of args for bess control C++ funcs
    Signed-off-by: Muhammad Asim Jamshed <muhammad.jamshed@intel.com>

--- cpiface/bess_control.h
+++ cpiface/bess_control.h
@@ -42,6 +42,42 @@ enum src_iface_type { Access = 1, Core };
 #define MODULE_NAME_LEN 128
 #define HOSTNAME_LEN 256
 /*--------------------------------------------------------------------------------*/
+struct PDRArgs {
+  enum src_iface_type sit; /* source iface */
+  uint32_t tipd;           /* tunnel_ipv4_dst */
+  uint32_t tipd_mask;      /* tunnel_ipv4_dst mask */
+  uint32_t enb_teid;       /* enb teid */
+  uint32_t enb_teid_mask;  /* enb teid mask */
+  uint32_t saddr;          /* src ip */
+  uint32_t saddr_mask;     /* src ip mask */
+  uint32_t daddr;          /* dest ip */
+  uint32_t daddr_mask;     /* dest ip mask */
+  uint16_t sport;          /* sport */
+  uint16_t sport_mask;     /* sport mask */
+  uint16_t dport;          /* dport */
+  uint16_t dport_mask;     /* dport mask */
+  uint8_t protoid;         /* proto id */
+  uint8_t protoid_mask;    /* proto id mask */
+  uint32_t pdr_id;         /* PDR ID */
+  uint32_t fseid;          /* FSEID */
+  uint32_t ctr_id;         /* Counter ID */
+  uint32_t far_id;         /* FAR ID */
+  uint8_t need_decap;
+};
+
+struct FARArgs {
+  uint32_t far_id;     /* FAR ID */
+  uint32_t fseid;      /* FSEID */
+  uint8_t tunnel;      /* needs tunnelling */
+  uint8_t drop;        /* drop action */
+  uint8_t notify_cp;   /* notify CP action */
+  uint16_t tuntype;    /* tunnel type */
+  uint32_t tun_src_ip; /* tunnel src IP addr */
+  uint32_t tun_dst_ip; /* tunnel dst IP addr */
+  uint32_t teid;       /* TEID */
+  uint16_t tun_port;   /* tunnel UDP port # */
+};
+/*--------------------------------------------------------------------------------*/
 class BessClient {
  private:
   std::unique_ptr<bess::pb::BESSControl::Stub> stub_;
@@ -53,17 +89,7 @@ class BessClient {
   BessClient(std::shared_ptr<Channel> channel)
       : stub_(bess::pb::BESSControl::NewStub(channel)), crt() {}
 
-  void runAddPDRCommand(const enum src_iface_type sit, const uint32_t tipd,
-                        const uint32_t tipd_mask, const uint32_t enb_teid,
-                        const uint32_t enb_teid_mask, const uint32_t ueaddr,
-                        const uint32_t ueaddr_mask, const uint32_t inetip,
-                        const uint32_t inetip_mask, const uint16_t ueport,
-                        const uint16_t ueport_mask, const uint16_t inetport,
-                        const uint16_t inetport_mask, const uint8_t protoid,
-                        const uint8_t protoid_mask, const uint32_t pdr_id,
-                        const uint32_t fseid, const uint32_t ctr_id,
-                        const uint32_t far_id, const uint8_t need_decap,
-                        const char *modname) {
+  void runAddPDRCommand(const PDRArgs *pa, const char *modname) {
     bess::pb::WildcardMatchCommandAddArg *wmcaa =
         new bess::pb::WildcardMatchCommandAddArg();
     wmcaa->set_gate(1);
@@ -73,35 +99,35 @@ class BessClient {
     /* set src_iface value */
     bess::pb::FieldData *src_iface = wmcaa->add_values();
     /* Access = 1, Core = 2 */
-    src_iface->set_value_int(sit);
+    src_iface->set_value_int(pa->sit);
 
     /* set tunnel_ipv4_dst */
     bess::pb::FieldData *tunnel_ipv4_dst = wmcaa->add_values();
-    tunnel_ipv4_dst->set_value_int(tipd);
+    tunnel_ipv4_dst->set_value_int(pa->tipd);
 
     /* set teid */
     bess::pb::FieldData *_teid = wmcaa->add_values();
-    _teid->set_value_int(enb_teid);
+    _teid->set_value_int(pa->enb_teid);
 
     /* set dst ip */
     bess::pb::FieldData *dst_ip = wmcaa->add_values();
-    dst_ip->set_value_int(ueaddr);
+    dst_ip->set_value_int(pa->saddr);
 
     /* set src ip */
     bess::pb::FieldData *src_ip = wmcaa->add_values();
-    src_ip->set_value_int(inetip);
+    src_ip->set_value_int(pa->daddr);
 
     /* set dst l4 port */
     bess::pb::FieldData *l4_dstport = wmcaa->add_values();
-    l4_dstport->set_value_int(ueport);
+    l4_dstport->set_value_int(pa->sport);
 
     /* set src l4 port */
     bess::pb::FieldData *l4_srcport = wmcaa->add_values();
-    l4_srcport->set_value_int(inetport);
+    l4_srcport->set_value_int(pa->dport);
 
     /* set proto id */
     bess::pb::FieldData *_protoid = wmcaa->add_values();
-    _protoid->set_value_int(protoid);
+    _protoid->set_value_int(pa->protoid);
 
     /* SET MASKS */
     /* set src_iface value */
@@ -111,52 +137,52 @@ class BessClient {
 
     /* set tunnel_ipv4_dst - Setting it to 0 for the time being */
     tunnel_ipv4_dst = wmcaa->add_masks();
-    tunnel_ipv4_dst->set_value_int(tipd_mask);
+    tunnel_ipv4_dst->set_value_int(pa->tipd_mask);
 
     /* set teid */
     _teid = wmcaa->add_masks();
-    _teid->set_value_int(enb_teid_mask);
+    _teid->set_value_int(pa->enb_teid_mask);
 
     /* set dst ip */
     dst_ip = wmcaa->add_masks();
-    dst_ip->set_value_int(ueaddr_mask);
+    dst_ip->set_value_int(pa->saddr_mask);
 
     /* set src ip */
     src_ip = wmcaa->add_masks();
-    src_ip->set_value_int(inetip_mask);
+    src_ip->set_value_int(pa->daddr_mask);
 
     /* set dst l4 port */
     l4_dstport = wmcaa->add_masks();
-    l4_dstport->set_value_int(ueport_mask);
+    l4_dstport->set_value_int(pa->sport_mask);
 
     /* set src l4 port */
     l4_srcport = wmcaa->add_masks();
-    l4_srcport->set_value_int(inetport_mask);
+    l4_srcport->set_value_int(pa->dport_mask);
 
     /* set proto id */
     _protoid = wmcaa->add_masks();
-    _protoid->set_value_int(protoid_mask);
+    _protoid->set_value_int(pa->protoid_mask);
 
     /* SET VALUESV */
     /* set pdr_id, set to 0 for the time being */
     bess::pb::FieldData *_void = wmcaa->add_valuesv();
-    _void->set_value_int(pdr_id);
+    _void->set_value_int(pa->pdr_id);
 
     /* set fseid, set to 0 for the time being */
     _void = wmcaa->add_valuesv();
-    _void->set_value_int(fseid);
+    _void->set_value_int(pa->fseid);
 
     /* set ctr_id, set to teid for the time being */
     _void = wmcaa->add_valuesv();
-    _void->set_value_int(ctr_id);
+    _void->set_value_int(pa->ctr_id);
 
     /* set far_id, set to 0 for the time being */
     _void = wmcaa->add_valuesv();
-    _void->set_value_int(far_id);
+    _void->set_value_int(pa->far_id);
 
     /* set needs_gtpu_decap, set to 0 for the time being */
     _void = wmcaa->add_valuesv();
-    _void->set_value_int(need_decap);
+    _void->set_value_int(pa->need_decap);
 
     ::google::protobuf::Any *any = new ::google::protobuf::Any();
     any->PackFrom(*wmcaa);
@@ -178,14 +204,7 @@ class BessClient {
     /* `any' freed up by ModuleCommand() */
   }
 
-  void runDelPDRCommand(const enum src_iface_type sit, const uint32_t tipd,
-                        const uint32_t tipd_mask, const uint32_t enb_teid,
-                        const uint32_t enb_teid_mask, const uint32_t ueaddr,
-                        const uint32_t ueaddr_mask, const uint32_t inetip,
-                        const uint32_t inetip_mask, const uint16_t ueport,
-                        const uint16_t ueport_mask, const uint16_t inetport,
-                        const uint16_t inetport_mask, const uint8_t protoid,
-                        const uint8_t protoid_mask, const char *modname) {
+  void runDelPDRCommand(const PDRArgs *pa, const char *modname) {
     bess::pb::WildcardMatchCommandDeleteArg *wmcda =
         new bess::pb::WildcardMatchCommandDeleteArg();
 
@@ -193,35 +212,35 @@ class BessClient {
     /* set src_iface value */
     bess::pb::FieldData *src_iface = wmcda->add_values();
     /* Access = 1, Core = 2 */
-    src_iface->set_value_int(sit);
+    src_iface->set_value_int(pa->sit);
 
     /* set tunnel_ipv4_dst */
     bess::pb::FieldData *tunnel_ipv4_dst = wmcda->add_values();
-    tunnel_ipv4_dst->set_value_int(tipd);
+    tunnel_ipv4_dst->set_value_int(pa->tipd);
 
     /* set teid */
     bess::pb::FieldData *_teid = wmcda->add_values();
-    _teid->set_value_int(enb_teid);
+    _teid->set_value_int(pa->enb_teid);
 
     /* set dst ip */
     bess::pb::FieldData *dst_ip = wmcda->add_values();
-    dst_ip->set_value_int(ueaddr);
+    dst_ip->set_value_int(pa->saddr);
 
     /* set src ip */
     bess::pb::FieldData *src_ip = wmcda->add_values();
-    src_ip->set_value_int(inetip);
+    src_ip->set_value_int(pa->daddr);
 
     /* set dst l4 port */
     bess::pb::FieldData *l4_dstport = wmcda->add_values();
-    l4_dstport->set_value_int(ueport);
+    l4_dstport->set_value_int(pa->sport);
 
     /* set src l4 port */
     bess::pb::FieldData *l4_srcport = wmcda->add_values();
-    l4_srcport->set_value_int(inetport);
+    l4_srcport->set_value_int(pa->dport);
 
     /* set proto id */
     bess::pb::FieldData *_protoid = wmcda->add_values();
-    _protoid->set_value_int(protoid);
+    _protoid->set_value_int(pa->protoid);
 
     /* SET MASKS */
     /* set src_iface value */
@@ -231,31 +250,31 @@ class BessClient {
 
     /* set tunnel_ipv4_dst - Setting it to 0 for the time being */
     tunnel_ipv4_dst = wmcda->add_masks();
-    tunnel_ipv4_dst->set_value_int(tipd_mask);
+    tunnel_ipv4_dst->set_value_int(pa->tipd_mask);
 
     /* set teid */
     _teid = wmcda->add_masks();
-    _teid->set_value_int(enb_teid_mask);
+    _teid->set_value_int(pa->enb_teid_mask);
 
     /* set dst ip */
     dst_ip = wmcda->add_masks();
-    dst_ip->set_value_int(ueaddr_mask);
+    dst_ip->set_value_int(pa->saddr_mask);
 
     /* set src ip */
     src_ip = wmcda->add_masks();
-    src_ip->set_value_int(inetip_mask);
+    src_ip->set_value_int(pa->daddr_mask);
 
     /* set dst l4 port */
     l4_dstport = wmcda->add_masks();
-    l4_dstport->set_value_int(ueport_mask);
+    l4_dstport->set_value_int(pa->sport_mask);
 
     /* set src l4 port */
     l4_srcport = wmcda->add_masks();
-    l4_srcport->set_value_int(inetport_mask);
+    l4_srcport->set_value_int(pa->sport_mask);
 
     /* set proto id */
     _protoid = wmcda->add_masks();
-    _protoid->set_value_int(protoid_mask);
+    _protoid->set_value_int(pa->protoid_mask);
 
     ::google::protobuf::Any *any = new ::google::protobuf::Any();
     any->PackFrom(*wmcda);
@@ -277,12 +296,7 @@ class BessClient {
     /* `any' freed up by ModuleCommand() */
   }
 
-  void runAddFARCommand(const uint32_t far_id, const uint32_t fseid,
-                        const uint8_t tunnel, const uint8_t drop,
-                        const uint8_t notify_cp, const uint16_t tuntype,
-                        const uint32_t tun_src_ip, const uint32_t tun_dst_ip,
-                        const uint32_t teid, const uint16_t tun_port,
-                        const char *modname) {
+  void runAddFARCommand(const FARArgs *fa, const char *modname) {
     bess::pb::ExactMatchCommandAddArg *emcaa =
         new bess::pb::ExactMatchCommandAddArg();
     emcaa->set_gate(1);
@@ -290,43 +304,43 @@ class BessClient {
     /* SET FIELDS */
     /* set far_id value */
     bess::pb::FieldData *_far_id = emcaa->add_fields();
-    _far_id->set_value_int(far_id);
+    _far_id->set_value_int(fa->far_id);
 
     /* set fseid */
     bess::pb::FieldData *_fseid = emcaa->add_fields();
-    _fseid->set_value_int(fseid);
+    _fseid->set_value_int(fa->fseid);
 
     /* SET VALUES */
     /* set FAR action */
     uint8_t action = 0;
-    if (tunnel)
+    if (fa->tunnel)
       action = action | DO_TUNNEL;
-    if (drop)
+    if (fa->drop)
       action = action | DO_DROP;
-    if (notify_cp)
+    if (fa->notify_cp)
       action = action | DO_NOTIFY;
     bess::pb::FieldData *_action = emcaa->add_values();
     _action->set_value_int(action);
 
     /* set tuntype */
     bess::pb::FieldData *_tuntype = emcaa->add_values();
-    _tuntype->set_value_int(tuntype);
+    _tuntype->set_value_int(fa->tuntype);
 
     /* set tun_src_ip */
     bess::pb::FieldData *_tun_src_ip = emcaa->add_values();
-    _tun_src_ip->set_value_int(tun_src_ip);
+    _tun_src_ip->set_value_int(fa->tun_src_ip);
 
     /* set tun_dst_ip */
     bess::pb::FieldData *_tun_dst_ip = emcaa->add_values();
-    _tun_dst_ip->set_value_int(tun_dst_ip);
+    _tun_dst_ip->set_value_int(fa->tun_dst_ip);
 
     /* set teid */
     bess::pb::FieldData *_teid = emcaa->add_values();
-    _teid->set_value_int(teid);
+    _teid->set_value_int(fa->teid);
 
     /* set tun_port */
     bess::pb::FieldData *_tun_port = emcaa->add_values();
-    _tun_port->set_value_int(tun_port);
+    _tun_port->set_value_int(fa->tun_port);
 
     ::google::protobuf::Any *any = new ::google::protobuf::Any();
     any->PackFrom(*emcaa);
@@ -348,19 +362,18 @@ class BessClient {
     /* `any' freed up by ModuleCommand() */
   }
 
-  void runDelFARCommand(const uint32_t far_id, const uint32_t fseid,
-                        const char *modname) {
+  void runDelFARCommand(const FARArgs *fa, const char *modname) {
     bess::pb::ExactMatchCommandDeleteArg *emcda =
         new bess::pb::ExactMatchCommandDeleteArg();
 
     /* SET FIELDS */
     /* set far_id value */
     bess::pb::FieldData *_far_id = emcda->add_fields();
-    _far_id->set_value_int(far_id);
+    _far_id->set_value_int(fa->far_id);
 
     /* set fseid */
     bess::pb::FieldData *_fseid = emcda->add_fields();
-    _fseid->set_value_int(fseid);
+    _fseid->set_value_int(fa->fseid);
 
     ::google::protobuf::Any *any = new ::google::protobuf::Any();
     any->PackFrom(*emcda);
