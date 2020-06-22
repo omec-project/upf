ef37ef6ec6cb4872a6d3730da8f070895d407474
Muhammad Asim Jamshed <muhammad.jamshed@intel.com>
2020-06-21 23:29:31 -0700

    Modularized the addPDR, addFAR, & addCounters func.
    PFCP-based cpiface should be able to use these functions now.
    
    Signed-off-by: Muhammad Asim Jamshed <muhammad.jamshed@intel.com>

--- pfcpiface/upf.go
+++ pfcpiface/upf.go
@@ -45,26 +45,38 @@ type pdr struct {
 	srcIface     uint8
 	tunnelIP4Dst uint32
 	eNBTeid      uint32
-	ueAddrIP     uint32
-	inetIP       uint32
-	uePort       uint16
-	inetPort     uint16
+	srcIP        uint32
+	dstIP        uint32
+	srcPort      uint16
+	dstPort      uint16
 	proto        uint8
 
 	srcIfaceMask     uint8
 	tunnelIP4DstMask uint32
 	eNBTeidMask      uint32
-	ueAddrIPMask     uint32
-	inetIPMask       uint32
-	uePortMask       uint16
-	inetPortMask     uint16
+	srcIPMask        uint32
+	dstIPMask        uint32
+	srcPortMask      uint16
+	dstPortMask      uint16
 	protoMask        uint8
 
 	pdrID     uint32
 	fseID     uint32
 	ctrID     uint32
-	farID     uint32
+	farID     uint8
 	needDecap uint8
+}
+
+type far struct {
+	farID uint8
+	fseID uint32
+
+	action      uint8
+	tunnelType  uint8
+	s1uIP       uint32
+	eNBIP       uint32
+	eNBTeid     uint32
+	UDPGTPUPort uint16
 }
 
 var _ pdr
@@ -90,39 +102,37 @@ func (u *upf) resumeAll() error {
 	return err
 }
 
-func (u *upf) addPDR(sessionID uint32) {
-
-	const ueip, teid = 0x10000001, 0xf0000000
+func (u *upf) addPDR(p pdr) {
 
 	f := &pb.WildcardMatchCommandAddArg{
 		Gate:     1,
 		Priority: 1,
 		Values: []*pb.FieldData{
-			intEnc(core),                     /* src_iface */
-			{},                               /* tunnel_ipv4_dst */
-			{},                               /* enb_teid */
-			intEnc(uint64(ueip + sessionID)), /* ueaddr ip*/
-			{},                               /* inet ip */
-			{},                               /* ue port */
-			{},                               /* inet port */
-			{},                               /* proto id */
+			intEnc(uint64(p.srcIface)),     /* src_iface */
+			intEnc(uint64(p.tunnelIP4Dst)), /* tunnel_ipv4_dst */
+			intEnc(uint64(p.eNBTeid)),      /* enb_teid */
+			intEnc(uint64(p.srcIP)),        /* ueaddr ip*/
+			intEnc(uint64(p.dstIP)),        /* inet ip */
+			intEnc(uint64(p.srcPort)),      /* ue port */
+			intEnc(uint64(p.dstPort)),      /* inet port */
+			intEnc(uint64(p.proto)),        /* proto id */
 		},
 		Masks: []*pb.FieldData{
-			intEnc(0xFF),       /* src_iface-mask */
-			{},                 /* tunnel_ipv4_dst-mask */
-			{},                 /* enb_teid-mask */
-			intEnc(0xFFFFFFFF), /* ueaddr ip-mask */
-			{},                 /* inet ip-mask */
-			{},                 /* ue port-mask */
-			{},                 /* inet port-mask */
-			{},                 /* proto id-mask */
+			intEnc(uint64(p.srcIfaceMask)),     /* src_iface-mask */
+			intEnc(uint64(p.tunnelIP4DstMask)), /* tunnel_ipv4_dst-mask */
+			intEnc(uint64(p.eNBTeidMask)),      /* enb_teid-mask */
+			intEnc(uint64(p.srcIPMask)),        /* ueaddr ip-mask */
+			intEnc(uint64(p.dstIPMask)),        /* inet ip-mask */
+			intEnc(uint64(p.srcPortMask)),      /* ue port-mask */
+			intEnc(uint64(p.dstPortMask)),      /* inet port-mask */
+			intEnc(uint64(p.protoMask)),        /* proto id-mask */
 		},
 		Valuesv: []*pb.FieldData{
-			{},                               /* pdr-id */
-			intEnc(uint64(teid + sessionID)), /* fseid */
-			intEnc(uint64(sessionID)),        /* ctr_id */
-			intEnc(uint64(downlink)),         /* far_id */
-			{},                               /* need_decap */
+			intEnc(uint64(p.pdrID)),     /* pdr-id */
+			intEnc(uint64(p.fseID)),     /* fseid */
+			intEnc(uint64(p.ctrID)),     /* ctr_id */
+			intEnc(uint64(p.farID)),     /* far_id */
+			intEnc(uint64(p.needDecap)), /* need_decap */
 		},
 	}
 
@@ -158,99 +168,23 @@ func (u *upf) addPDR(sessionID uint32) {
 			break
 		}
 	}
-
-	f = &pb.WildcardMatchCommandAddArg{
-		Gate:     1,
-		Priority: 1,
-		Values: []*pb.FieldData{
-			intEnc(access),                   /* src_iface */
-			{},                               /* tunnel_ipv4_dst */
-			intEnc(uint64(teid + sessionID)), /* enb_teid */
-			{},                               /* inet ip */
-			intEnc(uint64(ueip + sessionID)), /* ueaddr ip */
-			{},                               /* inet port */
-			{},                               /* ue port */
-			{},                               /* proto-id */
-		},
-		Masks: []*pb.FieldData{
-			intEnc(0xFF),       /* src_iface-mask */
-			{},                 /* tunnel_ipv4_dst-mask */
-			intEnc(0xFFFFFFFF), /* enb_teid-mask */
-			{},                 /* inet ip-mask */
-			intEnc(0xFFFFFFFF), /* ueaddr ip-mask */
-			{},                 /* inet port-mask */
-			{},                 /* ue port-mask */
-			{},                 /* proto-id-mask */
-		},
-		Valuesv: []*pb.FieldData{
-			{},                               /* pdr_id */
-			intEnc(uint64(teid + sessionID)), /* fseid */
-			intEnc(uint64(sessionID)),        /* ctr_id */
-			intEnc(uint64(uplink)),           /* far_id */
-			intEnc(0x1),                      /* need_decap */
-		},
-	}
-
-	any, err = ptypes.MarshalAny(f)
-	if err != nil {
-		log.Println("Error marshalling the rule", f, err)
-		return
-	}
-
-	for res, err := u.client.ModuleCommand(u.ctx, &pb.CommandRequest{
-		Name: "PDRLookup",
-		Cmd:  "add",
-		Arg:  any,
-	}); ; {
-
-		if err != nil {
-			// codes.DeadlineExceeded.String() has a diff string literal. WHY ?!!
-			if !strings.Contains(grpc.ErrorDesc(err), "deadline exceeded") {
-				log.Println("Error running module command:", err)
-				return
-			}
-			log.Println("Deadline exceeded when trying to insert UPLINK PDR rule! Retrying...")
-		}
-
-		if res.GetError() != nil {
-			log.Println("Error updating rule:", res.GetError())
-			return
-		}
-
-		/* everything went fine */
-		if err == nil {
-			break
-		}
-	}
 }
 
-func (u *upf) addFAR(sessionID uint32) {
-
-	const ueip, teid, enbip = 0x10000001, 0xf0000000, 0x0b010181
-	const ng4tMaxUeRan, ng4tMaxEnbRan = 500000, 80
-	s1uip := ip2int(u.n3IP)
-
-	// NG4T-based formula to calculate enodeB IP address against a given UE IP address
-	// il_trafficgen also uses the same scheme
-	// See SimuCPEnbv4Teid(...) in ngic code for more details
-	ueOfRan := sessionID % ng4tMaxUeRan
-	ran := sessionID / ng4tMaxUeRan
-	enbOfRan := ueOfRan % ng4tMaxEnbRan
-	enbIdx := ran*ng4tMaxEnbRan + enbOfRan
+func (u *upf) addFAR(far far) {
 
 	f := &pb.ExactMatchCommandAddArg{
 		Gate: 1,
 		Fields: []*pb.FieldData{
-			intEnc(uint64(downlink)),         /* far_id */
-			intEnc(uint64(teid + sessionID)), /* fseid */
+			intEnc(uint64(far.farID)), /* far_id */
+			intEnc(uint64(far.fseID)), /* fseid */
 		},
 		Values: []*pb.FieldData{
-			intEnc(farTunnel),                /* action */
-			intEnc(0x1),                      /* tunnel_out_type */
-			intEnc(uint64(s1uip)),            /* s1u-ip */
-			intEnc(uint64(enbip + enbIdx)),   /* enb ip */
-			intEnc(uint64(teid + sessionID)), /* enb teid */
-			intEnc(uint64(udpGTPUPort)),      /* udp gtpu port */
+			intEnc(uint64(far.action)),      /* action */
+			intEnc(uint64(far.tunnelType)),  /* tunnel_out_type */
+			intEnc(uint64(far.s1uIP)),       /* s1u-ip */
+			intEnc(uint64(far.eNBIP)),       /* enb ip */
+			intEnc(uint64(far.eNBTeid)),     /* enb teid */
+			intEnc(uint64(far.UDPGTPUPort)), /* udp gtpu port */
 		},
 	}
 
@@ -285,60 +219,12 @@ func (u *upf) addFAR(sessionID uint32) {
 			break
 		}
 	}
-
-	f = &pb.ExactMatchCommandAddArg{
-		Gate: 1,
-		Fields: []*pb.FieldData{
-			intEnc(uint64(uplink)),           /* far id */
-			intEnc(uint64(teid + sessionID)), /* fseid */
-		},
-		Values: []*pb.FieldData{
-			intEnc(farForward), /* action */
-			{},                 /* tunnel_out_type */
-			{},                 /* not needed */
-			{},                 /* not needed */
-			{},                 /* not needed */
-			{},                 /* not needed */
-		},
-	}
-
-	any, err = ptypes.MarshalAny(f)
-	if err != nil {
-		log.Println("Error marshalling the rule", f, err)
-		return
-	}
-
-	for res, err := u.client.ModuleCommand(u.ctx, &pb.CommandRequest{
-		Name: "FARLookup",
-		Cmd:  "add",
-		Arg:  any,
-	}); ; {
-
-		if err != nil {
-			// codes.DeadlineExceeded.String() has a diff string literal. WHY ?!!
-			if !strings.Contains(grpc.ErrorDesc(err), "deadline exceeded") {
-				log.Println("Error running module command:", err)
-				return
-			}
-			log.Println("Deadline exceeded when trying to insert UPLINK FAR rule! Retrying...")
-		}
-
-		if res.GetError() != nil {
-			log.Println("Error updating rule:", res.GetError())
-			return
-		}
-
-		/* everything went fine */
-		if err == nil {
-			break
-		}
-	}
 }
 
-func (u *upf) addCounters(sessionID uint32) {
+func (u *upf) addCounters(ctrID uint32) {
 
 	f := &pb.CounterAddArg{
-		CtrId: sessionID,
+		CtrId: ctrID,
 	}
 
 	any, err := ptypes.MarshalAny(f)
@@ -374,7 +260,7 @@ func (u *upf) addCounters(sessionID uint32) {
 	}
 
 	f = &pb.CounterAddArg{
-		CtrId: sessionID,
+		CtrId: ctrID,
 	}
 
 	any, err = ptypes.MarshalAny(f)
@@ -410,7 +296,7 @@ func (u *upf) addCounters(sessionID uint32) {
 	}
 
 	f = &pb.CounterAddArg{
-		CtrId: sessionID,
+		CtrId: ctrID,
 	}
 
 	any, err = ptypes.MarshalAny(f)
