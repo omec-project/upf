8cf3134255e0295341eba2098c99176853035211
Muhammad Asim Jamshed <muhammad.jamshed@intel.com>
2020-07-17 17:58:06 -0700

    ZMQ-based cpiface changes for new UPF dataplane.
    Signed-off-by: Muhammad Asim Jamshed <muhammad.jamshed@intel.com>
    Co-authored-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

--- cpiface/zmq-cpiface.cc
+++ cpiface/zmq-cpiface.cc
@@ -3,28 +3,27 @@
  * Copyright(c) 2019 Intel Corporation
  */
 
+/* for bess-based GRPC funcs */
 #include "bess_control.h"
-#include <arpa/inet.h>
+/* for parsing */
+#include "parser.h"
 #include <ctime>
-#include <getopt.h>
+/* for stack iterator */
 #include <iterator>
+/* for session map*/
 #include <map>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <sys/time.h>
+/* for stack */
+#include <stack>
 #include <sys/types.h>
+/* for exec wait */
 #include <sys/wait.h>
 #include <unistd.h>
+/* for libzmq */
 #include <zmq.h>
-/*--------------------------------------------------------------------------------*/
-#define ZMQ_SERVER_IP "172.17.0.2"
-#define ZMQ_RECV_PORT 20
-#define ZMQ_SEND_PORT 5557
-#define ZMQ_NB_IP "172.17.0.1"
-#define ZMQ_NB_PORT 21
-#define S1U_SGW_IP "11.1.1.1"
-#define ZMQ_POLL_TIMEOUT 1000  // in msecs
-#define KEEPALIVE_TIMEOUT 100  // in secs
+/* for gettimeofday */
+#include <sys/time.h>
+/* for templates */
+#include "template.h"
 /*--------------------------------------------------------------------------------*/
 /**
  * ZMQ stuff
@@ -35,106 +34,12 @@ void *reg;
 void *context0;
 void *context1;
 void *context2;
-/*--------------------------------------------------------------------------------*/
-struct Args {
-  char bessd_ip[HOSTNAME_LEN] = BESSD_IP;
-  char zmqd_ip[HOSTNAME_LEN] = ZMQ_SERVER_IP;
-  char zmqd_nb_ip[HOSTNAME_LEN] = ZMQ_NB_IP;
-  char s1u_sgw_ip[HOSTNAME_LEN] = S1U_SGW_IP;
-  uint16_t bessd_port = BESSD_PORT;
-  uint16_t zmqd_send_port = ZMQ_SEND_PORT;
-  uint16_t zmqd_recv_port = ZMQ_RECV_PORT;
-  uint16_t zmqd_nb_port = ZMQ_NB_PORT;
-  char encapmod[MODULE_NAME_LEN] = ENCAPMOD;
+#define ZMQ_POLL_TIMEOUT 1000  // in msecs
+#define KEEPALIVE_TIMEOUT 100  // in secs
 
-  struct RegMsgBundle {
-    struct in_addr upf_comm_ip;
-    struct in_addr s1u_ip;
-    char hostname[HOSTNAME_LEN];
-  } rmb = {{.s_addr = 0}, {.s_addr = 0}, ""};
-  void parse(const int argc, char **argv) {
-    int c;
-    // Get args from command line
-    static const struct option long_options[] = {
-        {"bessd_ip", required_argument, NULL, 'B'},
-        {"bessd_port", required_argument, NULL, 'b'},
-        {"zmqd_ip", required_argument, NULL, 'Z'},
-        {"zmqd_send_port", required_argument, NULL, 's'},
-        {"zmqd_recv_port", required_argument, NULL, 'r'},
-        {"zmqd_nb_ip", required_argument, NULL, 'N'},
-        {"zmqd_nb_port", required_argument, NULL, 'n'},
-        {"s1u_sgw_ip", required_argument, NULL, 'u'},
-        {"encapmod", required_argument, NULL, 'M'},
-        {"hostname", required_argument, NULL, 'h'},
-        {0, 0, 0, 0}};
-    do {
-      int option_index = 0;
-      uint32_t val = 0;
-
-      c = getopt_long(argc, argv, "B:b:Z:s:r:M:N:n:u:h:", long_options,
-                      &option_index);
-
-      if (c == -1)
-        break;
-
-      switch (c) {
-        case 'B':
-          strncpy(bessd_ip, optarg, MIN(strlen(optarg), HOSTNAME_LEN - 1));
-          break;
-        case 'b':
-          val = strtoul(optarg, NULL, 10);
-          if (val == ULONG_MAX && errno == ERANGE) {
-            std::cerr << "Failed to parse bessd_port" << std::endl;
-            exit(EXIT_FAILURE);
-          }
-          bessd_port = (uint16_t)(val & 0x0000FFFF);
-          break;
-        case 'Z':
-          strncpy(zmqd_ip, optarg, MIN(strlen(optarg), HOSTNAME_LEN - 1));
-          break;
-        case 's':
-          val = strtoul(optarg, NULL, 10);
-          if (val == ULONG_MAX && errno == ERANGE) {
-            std::cerr << "Failed to parse zmqd_send_port" << std::endl;
-            exit(EXIT_FAILURE);
-          }
-          zmqd_send_port = (uint16_t)(val & 0x0000FFFF);
-          break;
-        case 'r':
-          val = strtoul(optarg, NULL, 10);
-          if (val == ULONG_MAX && errno == ERANGE) {
-            std::cerr << "Failed to parse zmqd_recv_port" << std::endl;
-            exit(EXIT_FAILURE);
-          }
-          zmqd_recv_port = (uint16_t)(val & 0x0000FFFF);
-          break;
-        case 'M':
-          strncpy(encapmod, optarg, MIN(strlen(optarg), MODULE_NAME_LEN - 1));
-          break;
-        case 'N':
-          strncpy(zmqd_nb_ip, optarg, MIN(strlen(optarg), HOSTNAME_LEN - 1));
-          break;
-        case 'n':
-          val = strtoul(optarg, NULL, 10);
-          if (val == ULONG_MAX && errno == ERANGE) {
-            std::cerr << "Failed to parse zmqd_nb_port" << std::endl;
-            exit(EXIT_FAILURE);
-          }
-          zmqd_nb_port = (uint16_t)(val & 0x0000FFFF);
-          break;
-        case 'u':
-          strncpy(s1u_sgw_ip, optarg, MIN(strlen(optarg), HOSTNAME_LEN - 1));
-          break;
-        case 'h':
-          strncpy(rmb.hostname, optarg, MIN(strlen(optarg), HOSTNAME_LEN - 1));
-          break;
-        default:
-          std::cerr << "Unknown argument - " << argv[optind] << std::endl;
-          exit(EXIT_FAILURE);
-          break;
-      }
-    } while (c != -1);
-  }
+struct TeidEntry {
+  uint32_t teid;
+  uint32_t ctr_id;
 };
 /*--------------------------------------------------------------------------------*/
 void sig_handler(int signo) {
@@ -179,10 +84,21 @@ void force_restart(int argc, char **argv) {
   }
 }
 /*--------------------------------------------------------------------------------*/
+void invokeGRPCall(Args args, void *func_args, const char *modname,
+                   uint8_t funcID) {
+  BessClient b(CreateChannel(
+      std::string(args.bessd_ip) + ":" + std::to_string(args.bessd_port),
+      InsecureChannelCredentials()));
+  ((b).*(b.grpc_ptr[funcID]))(func_args, modname);
+}
+/*--------------------------------------------------------------------------------*/
 int main(int argc, char **argv) {
   GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-  std::map<uint64_t, bool> zmq_sess_map;
+  /* key: SESS_ID(rbuf.sess_entry.ue_addr.u.ipv4_addr, DEFAULT_BEARER), val:
+   * enb_teid) */
+  std::map<uint64_t, TeidEntry> zmq_sess_map;
+  std::stack<uint32_t> counter;
   // set my_dp_id to 0, SPGW-C will give me the id
   uint32_t my_dp_id = 0;
   struct timeval last_ack, current_time;
@@ -190,7 +106,6 @@ int main(int argc, char **argv) {
   const uint32_t dp_cp_timeout_interval = KEEPALIVE_TIMEOUT;
   // 1 second zmq_poll timeout
   const uint32_t zmq_poll_timeout = ZMQ_POLL_TIMEOUT;
-  // set default values first
   Args args;
 
   context0 = zmq_ctx_new();
@@ -198,6 +113,10 @@ int main(int argc, char **argv) {
   context2 = zmq_ctx_new();
   // set args coming from command-line
   args.parse(argc, argv);
+
+  /* initialize stack */
+  for (int32_t k = args.counter_count - 1; k >= 0; k--)
+    counter.push(k);
 
   if (context0 == NULL || context1 == NULL || context2 == NULL) {
     std::cerr << "Failed to create context(s)!: " << strerror(errno)
@@ -214,7 +133,7 @@ int main(int argc, char **argv) {
   }
 
   // connect to registration port
-  if (zmq_connect(reg, ("tcp://" + std::string(args.zmqd_nb_ip) + ":" +
+  if (zmq_connect(reg, ("tcp://" + std::string(args.nb_dst_ip) + ":" +
                         std::to_string(args.zmqd_nb_port))
                            .c_str()) < 0) {
     std::cerr << "Failed to connect to registration port!: " << strerror(errno)
@@ -225,8 +144,8 @@ int main(int argc, char **argv) {
   VLOG(1) << "Connected to registration handle" << std::endl;
 
   // Build message
-  if (inet_aton(args.zmqd_ip, &args.rmb.upf_comm_ip) == 0) {
-    std::cerr << "Invalid address: " << args.zmqd_ip << std::endl;
+  if (inet_aton(args.nb_src_ip, &args.rmb.upf_comm_ip) == 0) {
+    std::cerr << "Invalid address: " << args.nb_src_ip << std::endl;
     return EXIT_FAILURE;
   }
   // set S1U IP address
@@ -234,11 +153,10 @@ int main(int argc, char **argv) {
     std::cerr << "Invalid address: " << args.s1u_sgw_ip << std::endl;
     return EXIT_FAILURE;
   }
-  // retrieve hostname
-  if (!strcmp(args.rmb.hostname, "") &&
-      gethostname(args.rmb.hostname, sizeof(args.rmb.hostname)) == -1) {
-    std::cerr << "Unable to retreive hostname of DP!" << std::endl;
-    return EXIT_FAILURE;
+  // retrieve hostname (along with domain name), incase json conf
+  // did not give us anything
+  if (!strcmp(args.rmb.hostname, "")) {
+    args.fetchHostname(args.rmb.hostname, sizeof(args.rmb.hostname));
   }
 
   VLOG(1) << "DP hostname: " << args.rmb.hostname << std::endl;
@@ -270,7 +188,7 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
   // Socket to recv message from
-  if (zmq_bind(receiver, ("tcp://" + std::string(args.zmqd_ip) + ":" +
+  if (zmq_bind(receiver, ("tcp://" + std::string(args.nb_src_ip) + ":" +
                           std::to_string(args.zmqd_recv_port))
                              .c_str()) != 0) {
     std::cerr << "Failed to bind to receiver ZMQ port!: " << strerror(errno)
@@ -279,7 +197,7 @@ int main(int argc, char **argv) {
   }
   //  Socket to send messages to
   sender = zmq_socket(context2, ZMQ_PUSH);
-  if (zmq_connect(sender, ("tcp://" + std::string(args.zmqd_nb_ip) + ":" +
+  if (zmq_connect(sender, ("tcp://" + std::string(args.nb_dst_ip) + ":" +
                            std::to_string(args.zmqd_send_port))
                               .c_str()) < 0) {
     std::cerr << "Failed to connect to sender!: " << strerror(errno)
@@ -332,6 +250,9 @@ int main(int argc, char **argv) {
         break;
       }
       long mtype = rbuf.mtype;
+      uint32_t enb_teid = 0;
+      uint32_t curr_ctr = 0;
+      TeidEntry te;
       memset(&resp, 0, sizeof(struct resp_msgbuf));
       switch (mtype) {
         case MSG_SESS_CRE:
@@ -348,9 +269,16 @@ int main(int argc, char **argv) {
           my_dp_id = rbuf.dp_id.id;
           resp.dp_id.id = rbuf.dp_id.id;
           resp.mtype = DPN_RESPONSE;
+          te.teid = 0;
+          te.ctr_id = counter.top();
           zmq_sess_map[SESS_ID(rbuf.sess_entry.ue_addr.u.ipv4_addr,
-                               DEFAULT_BEARER)] = true;
+                               DEFAULT_BEARER)] = te;
+          VLOG(1) << "Assigning sess with IP addr: "
+                  << rbuf.sess_entry.ue_addr.u.ipv4_addr
+                  << " counter: " << te.ctr_id << std::endl;
           resp.sess_id = rbuf.sess_entry.sess_id;
+          /* ctr_id is used up */
+          counter.pop();
           break;
         case MSG_SESS_MOD:
           VLOG(1) << "Got a session modify request, ";
@@ -370,18 +298,58 @@ int main(int argc, char **argv) {
               zmq_sess_map.end()) {
             std::cerr << "No record found!" << std::endl;
             break;
+          } else {
+            curr_ctr = zmq_sess_map[SESS_ID(rbuf.sess_entry.ue_addr.u.ipv4_addr,
+                                            DEFAULT_BEARER)]
+                           .ctr_id;
+            te.teid = rbuf.sess_entry.dl_s1_info.enb_teid;
+            te.ctr_id = curr_ctr;
+            zmq_sess_map[SESS_ID(rbuf.sess_entry.ue_addr.u.ipv4_addr,
+                                 DEFAULT_BEARER)] = te;
+            VLOG(1) << "Assigning sess with IP addr: "
+                    << rbuf.sess_entry.ue_addr.u.ipv4_addr
+                    << " and teid: " << te.teid << " counter: " << te.ctr_id
+                    << std::endl;
           }
-          {
-            // Create BessClient
-            BessClient b(CreateChannel(std::string(args.bessd_ip) + ":" +
-                                           std::to_string(args.bessd_port),
-                                       InsecureChannelCredentials()));
-            b.runAddCommand(rbuf.sess_entry.ul_s1_info.sgw_teid,
-                            rbuf.sess_entry.dl_s1_info.enb_teid,
-                            rbuf.sess_entry.ue_addr.u.ipv4_addr,
-                            rbuf.sess_entry.ul_s1_info.enb_addr.u.ipv4_addr,
-                            args.encapmod);
-          }
+          pdrD.saddr = rbuf.sess_entry.ue_addr.u.ipv4_addr; /* ueaddr ip */
+          pdrD.fseid = rbuf.sess_entry.dl_s1_info.enb_teid; /* fseid */
+          pdrD.ctr_id = curr_ctr;                           /* ctr_id */
+          // Add PDR (DOWNLINK)
+          invokeGRPCall(args, &pdrD, args.pdrlookup, GRPC_PDR_ADD);
+
+          pdrU.daddr = rbuf.sess_entry.ue_addr.u.ipv4_addr; /* ueaddr ip */
+          pdrU.fseid = rbuf.sess_entry.dl_s1_info.enb_teid; /* fseid */
+          pdrU.ctr_id = curr_ctr;                           /* ctr_id */
+          // Add PDR (UPLINK)
+          invokeGRPCall(args, &pdrU, args.pdrlookup, GRPC_PDR_ADD);
+
+          farD.fseid = rbuf.sess_entry.dl_s1_info.enb_teid; /* fseid */
+          farD.tun_src_ip =
+              ntohl((uint32_t)(inet_addr(args.s1u_sgw_ip))); /* n3 addr */
+          farD.tun_dst_ip =
+              rbuf.sess_entry.ul_s1_info.enb_addr.u.ipv4_addr; /* enb addr */
+          farD.teid = rbuf.sess_entry.dl_s1_info.enb_teid;     /* enb_teid */
+          // Add FAR (DOWNLINK)
+          invokeGRPCall(args, &farD, args.farlookup, GRPC_FAR_ADD);
+
+          farU.fseid = rbuf.sess_entry.dl_s1_info.enb_teid; /* fseid */
+          // Add FAR (UPLINK)
+          invokeGRPCall(args, &farU, args.farlookup, GRPC_FAR_ADD);
+
+          // Add PreQoS Counter
+          invokeGRPCall(args, (&curr_ctr),
+                        (("pre" + std::string(args.qoscounter)).c_str()),
+                        GRPC_CTR_ADD);
+
+          // Add PostQoS Counter
+          invokeGRPCall(args, (&curr_ctr),
+                        (("postUL" + std::string(args.qoscounter)).c_str()),
+                        GRPC_CTR_ADD);
+
+          // Add PostQoS Counter
+          invokeGRPCall(args, (&curr_ctr),
+                        (("postDL" + std::string(args.qoscounter)).c_str()),
+                        GRPC_CTR_ADD);
           break;
         case MSG_SESS_DEL:
           VLOG(1) << "Got a session delete request" << std::endl;
@@ -403,18 +371,63 @@ int main(int argc, char **argv) {
               zmq_sess_map.end()) {
             std::cerr << "No record found!" << std::endl;
             break;
+          } else {
+            enb_teid =
+                zmq_sess_map[SESS_ID((rbuf.sess_entry.ue_addr.u.ipv4_addr),
+                                     DEFAULT_BEARER)]
+                    .teid;
+            curr_ctr =
+                zmq_sess_map[SESS_ID((rbuf.sess_entry.ue_addr.u.ipv4_addr),
+                                     DEFAULT_BEARER)]
+                    .ctr_id;
+            VLOG(1) << "Assigning sess with IP addr: "
+                    << (rbuf.sess_entry.ue_addr.u.ipv4_addr)
+                    << " and teid: " << enb_teid << " counter: " << curr_ctr
+                    << std::endl;
           }
           {
-            // Create BessClient
-            BessClient b(CreateChannel(std::string(args.bessd_ip) + ":" +
-                                           std::to_string(args.bessd_port),
-                                       InsecureChannelCredentials()));
-            b.runRemoveCommand((rbuf.sess_entry.ue_addr.u.ipv4_addr),
-                               args.encapmod);
-            std::map<std::uint64_t, bool>::iterator it = zmq_sess_map.find(
+            std::map<std::uint64_t, TeidEntry>::iterator it = zmq_sess_map.find(
                 SESS_ID((rbuf.sess_entry.ue_addr.u.ipv4_addr), DEFAULT_BEARER));
             zmq_sess_map.erase(it);
           }
+
+          pdrD.saddr = (rbuf.sess_entry.ue_addr.u.ipv4_addr); /* ueaddr ip */
+          // Delete PDR (DOWNLINK)
+          invokeGRPCall(args, &pdrD, args.pdrlookup, GRPC_PDR_DEL);
+
+          pdrU.daddr = (rbuf.sess_entry.ue_addr.u.ipv4_addr); /* ueaddr ip */
+          // Delete PDR (UPLINK)
+          invokeGRPCall(args, &pdrU, args.pdrlookup, GRPC_PDR_DEL);
+
+          // Del FAR (DOWNLINK)
+          FARArgs fa;
+          fa.far_id = 1;
+          fa.fseid = enb_teid;
+          invokeGRPCall(args, &fa, args.farlookup, GRPC_FAR_DEL);
+
+          // Del FAR (UPLINK)
+          fa.far_id = 0;
+          fa.fseid = enb_teid;
+          invokeGRPCall(args, &fa, args.farlookup, GRPC_FAR_DEL);
+
+          // Delete PreQoS Counter
+          invokeGRPCall(args, (&curr_ctr),
+                        (("pre" + std::string(args.qoscounter)).c_str()),
+                        GRPC_CTR_DEL);
+
+          // Delete PostQoS Counter
+          invokeGRPCall(args, (&curr_ctr),
+                        (("postUL" + std::string(args.qoscounter)).c_str()),
+                        GRPC_CTR_DEL);
+
+          // Delete PostQoS Counter
+          invokeGRPCall(args, (&curr_ctr),
+                        (("postDL" + std::string(args.qoscounter)).c_str()),
+                        GRPC_CTR_DEL);
+
+          /* freed up counter id is returned to the stack */
+          VLOG(1) << "Curr Ctr returned: " << curr_ctr << std::endl;
+          counter.push(curr_ctr);
           break;
         case MSG_KEEPALIVE_ACK:
           my_dp_id = rbuf.dp_id.id;
@@ -441,14 +454,18 @@ int main(int argc, char **argv) {
       VLOG(1) << "ZMQ poll timeout DPID " << my_dp_id << std::endl;
       gettimeofday(&current_time, NULL);
       if (current_time.tv_sec - last_ack.tv_sec > dp_cp_timeout_interval) {
-        {
-          // Before restarting, delete all session records
-          // Create BessClient
-          BessClient b(CreateChannel(std::string(args.bessd_ip) + ":" +
-                                         std::to_string(args.bessd_port),
-                                     InsecureChannelCredentials()));
-          b.runRemoveAllCommand(args.encapmod);
-        }
+        invokeGRPCall(args, NULL, args.pdrlookup, GRPC_PDR_CLR);
+        invokeGRPCall(args, NULL, args.farlookup, GRPC_FAR_CLR);
+        invokeGRPCall(args, NULL,
+                      (("pre" + std::string(args.qoscounter)).c_str()),
+                      GRPC_CTR_CLR);
+        invokeGRPCall(args, NULL,
+                      (("postUL" + std::string(args.qoscounter)).c_str()),
+                      GRPC_CTR_CLR);
+        invokeGRPCall(args, NULL,
+                      (("postDL" + std::string(args.qoscounter)).c_str()),
+                      GRPC_CTR_CLR);
+
         std::cerr << "CP<-->DP communication broken. DPID: " << my_dp_id
                   << ". DP is restarting..." << std::endl;
         force_restart(argc, argv);
