94a0536958ba0b6fa615bd5e2adf272956f4927e
Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
2020-10-15 12:08:26 +0530

    more functions
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

--- pfcpiface/parser.go
+++ pfcpiface/parser.go
@@ -4,46 +4,130 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"net"
+	"strconv"
 	"strings"
 
 	"github.com/wmnsk/go-pfcp/ie"
 )
 
-func parseFlowDesc(flowDesc string) (IP4AddressNet string) {
+type endpoint struct {
+	IPNet *net.IPNet
+	Port  uint16
+}
+
+func (ep *endpoint) parseNet(ipnet string) error {
+	ipNetFields := strings.Split(ipnet, "/")
+	if len(ipNetFields) == 1 {
+		ipnet = ipNetFields[0] + "/32"
+	} else {
+		return errors.New("Incorrect network string")
+	}
 
 	var err error
-	var prefix string
-
-	token := strings.Split(flowDesc, " ")
-	//log.Println("IP Full String is:", token[4])
-	if token[4] == "" {
-		return "0.0.0.0/0"
+	_, ep.IPNet, err = net.ParseCIDR(ipnet)
+	if err != nil {
+		return errors.New("Unable to ParseCIDR")
 	}
-	addrString := strings.Split(token[4], "/")
-	for i := range addrString {
-		switch i {
-		case 0:
-			IP4AddressNet = addrString[i]
-			if IP4AddressNet == "any" {
-				return "0.0.0.0/0"
+	return nil
+}
+
+func (ep *endpoint) parsePort(port string) error {
+	p, err := strconv.ParseUint(port, 10, 16)
+	if err != nil {
+		return err
+	}
+	ep.Port = uint16(p)
+	return nil
+}
+
+type ipFilterRule struct {
+	action, direction string
+	proto             uint8
+	src, dst          endpoint
+}
+
+// "permit out ip from any to assigned"
+// "permit out ip from 60.60.0.102 to assigned"
+// "permit out ip from 60.60.0.102/32 to assigned"
+// "permit out ip from any to 60.60.0.102"
+// "permit out ip from 60.60.0.1/26 to 60.60.0.102"
+// "permit out ip from 60.60.0.1 8888 to 60.60.0.102/26"
+// "permit out ip from 60.60.0.1 to 60.60.0.102 9999"
+// "permit out ip from 60.60.0.1 8888 to 60.60.0.102 9999"
+
+func (ipf *ipFilterRule) parseFlowDesc(flowDesc, ueIP string) error {
+	fields := strings.Fields(flowDesc)
+	fmt.Println("Number of fields", len(fields))
+
+	ipf.action = fields[0]
+	ipf.direction = fields[1]
+
+	if err := ipf.parseProto(fields[2]); err != nil {
+		return err
+	}
+
+	// bring to common intermediate representation
+	xform := func(i int) {
+		switch fields[i] {
+		case "any":
+			fields[i] = "0.0.0.0/0"
+		case "assigned":
+			if ueIP != "" {
+				fields[i] = ueIP
+			} else {
+				fields[i] = "0.0.0.0/0"
 			}
-		case 1:
-			prefix = addrString[i]
-			if err != nil {
-				return IP4AddressNet + "/32"
-			}
-		default:
-			// do nothing
 		}
 	}
 
-	return IP4AddressNet + "/" + prefix
+	for i := 3; i < len(fields); i++ {
+		f := fields[i]
+		switch f {
+		case "from":
+			i++
+			xform(i)
+			err := ipf.src.parseNet(fields[i])
+			if err != nil {
+				log.Println(err)
+			}
+
+			if fields[i+1] != "to" {
+				i++
+				ipf.src.parsePort(fields[i])
+			}
+		case "to":
+			i++
+			xform(i)
+			err := ipf.dst.parseNet(fields[i])
+			if err != nil {
+				log.Println(err)
+			}
+
+			if i < len(fields)-1 {
+				i++
+				ipf.dst.parsePort(fields[i])
+			}
+		}
+	}
+
+	fmt.Println(ipf)
+	return nil
+}
+
+func (ipf *ipFilterRule) parseProto(proto string) error {
+	p, err := strconv.ParseUint(proto, 10, 16)
+	if err != nil {
+		return errors.New("Unable to parse proto")
+	}
+	ipf.proto = uint8(p)
+	return nil
 }
 
 func parseCreatePDRPDI(pdi []*ie.IE) *pdr {
-
 	var pdrI pdr
 	for _, ie2 := range pdi {
 		switch ie2.Type {
@@ -84,16 +168,18 @@ func parseCreatePDRPDI(pdi []*ie.IE) *pdr {
 		}
 	}
 
+	var ueIP4 net.IP
+
 	for _, ie2 := range pdi {
 		switch ie2.Type {
 		case ie.UEIPAddress:
-			ueip4, err := ie2.UEIPAddress()
+			ueIPaddr, err := ie2.UEIPAddress()
 			if err != nil {
 				log.Println("Failed to parse UE IP address")
 				continue
 			}
 
-			ueIP4 := ueip4.IPv4Address
+			ueIP4 = ueIPaddr.IPv4Address
 			// uplink PDR may not have UE IP address IE
 			// FIXIT/TODO/XXX Move this inside SrcInterfaceAccess IE check??
 			var ueIP uint32
@@ -126,32 +212,25 @@ func parseCreatePDRPDI(pdi []*ie.IE) *pdr {
 				// TODO: Implement referencing SDF ID
 				continue
 			}
-			inetIP4Address := parseFlowDesc(flowDesc)
-			log.Println("Flow Description is:", inetIP4Address)
-
-			dstIP4, dstIP4Net, err := net.ParseCIDR(inetIP4Address)
+			var ipf ipFilterRule
+			err = ipf.parseFlowDesc(flowDesc, ueIP4.String())
 			if err != nil {
-				log.Println("Failed to parse inet IP address!, inetIP4Address:", inetIP4Address)
-				// TODO: remove continue and return nil to signal error
-				// once spgw-c is updated
-				continue
-				//return nil
+				log.Println("Failed to parse flow desc:", err)
 			}
+			log.Println("Flow Description is:", ipf)
 
-			var dstIP uint32
-			var dstIPMask uint32
-
-			if dstIP4.String() != "0.0.0.0" {
-				dstIP = ip2int(dstIP4)
-				dstIPMask = ipMask2int(dstIP4Net.Mask)
-			}
+			pdrI.proto = ipf.proto
 
 			if pdrI.srcIface == access {
-				pdrI.dstIP = dstIP
-				pdrI.dstIPMask = dstIPMask
+				pdrI.dstIP = ip2int(ipf.dst.IPNet.IP)
+				pdrI.dstIPMask = ipMask2int(ipf.dst.IPNet.Mask)
+				pdrI.srcIP = ip2int(ipf.src.IPNet.IP)
+				pdrI.srcIPMask = ipMask2int(ipf.src.IPNet.Mask)
 			} else if pdrI.srcIface == core {
-				pdrI.srcIP = dstIP
-				pdrI.srcIPMask = dstIPMask
+				pdrI.srcIP = ip2int(ipf.dst.IPNet.IP)
+				pdrI.srcIPMask = ipMask2int(ipf.dst.IPNet.Mask)
+				pdrI.dstIP = ip2int(ipf.src.IPNet.IP)
+				pdrI.dstIPMask = ipMask2int(ipf.src.IPNet.Mask)
 			}
 		}
 	}
@@ -160,7 +239,6 @@ func parseCreatePDRPDI(pdi []*ie.IE) *pdr {
 }
 
 func parseCreatePDR(ie1 *ie.IE, seid uint64) *pdr {
-
 	/* reset outerHeaderRemoval to begin with */
 	outerHeaderRemoval := uint8(0)
 
