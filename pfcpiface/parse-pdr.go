1ca0cf9ec64f09d71ac5fc972230e4797026150f
Saikrishna Edupuganti <krsna1729@users.noreply.github.com>
2020-10-22 23:32:05 +0530

    Refactor pfcp processing for lib updates (#153)
    * WIP: Refactor pfcp processing for lib updates
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * more functions
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * Bug fix
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * break parser and session state modification into separte files
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * add update FAR call. dont error on parsePDR yet
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * return with errBadSDF and ignore
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * Check for apply action, cleanup redundant errors
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * bug fix FAR action
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * minor cleanup
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * Add remove pdr far
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * need to actually store the value in the map
    
    (facepalm)
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * Temporary debug
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * return if calls is zero
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * mod resp should have the right seid
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * Fix: Set GTPU udp port
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * Set UE IP in the pdr in case badSDF
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
    
    * Add copyright
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

---
+++ pfcpiface/parse-pdr.go
@@ -0,0 +1,219 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright(c) 2020 Intel Corporation
+
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/wmnsk/go-pfcp/ie"
+)
+
+type pdr struct {
+	srcIface     uint8
+	tunnelIP4Dst uint32
+	tunnelTEID   uint32
+	srcIP        uint32
+	dstIP        uint32
+	srcPort      uint16
+	dstPort      uint16
+	proto        uint8
+
+	srcIfaceMask     uint8
+	tunnelIP4DstMask uint32
+	tunnelTEIDMask   uint32
+	srcIPMask        uint32
+	dstIPMask        uint32
+	srcPortMask      uint16
+	dstPortMask      uint16
+	protoMask        uint8
+
+	precedence uint32
+	pdrID      uint32
+	fseID      uint32
+	ctrID      uint32
+	farID      uint8
+	needDecap  uint8
+}
+
+func (p *pdr) printPDR() {
+	log.Println("------------------ PDR ---------------------")
+	log.Println("Src Iface:", p.srcIface)
+	log.Println("tunnelIP4Dst:", int2ip(p.tunnelIP4Dst))
+	log.Println("tunnelTEID:", p.tunnelTEID)
+	log.Println("srcIP:", int2ip(p.srcIP))
+	log.Println("dstIP:", int2ip(p.dstIP))
+	log.Println("srcPort:", p.srcPort)
+	log.Println("dstPort:", p.dstPort)
+	log.Println("proto:", p.proto)
+	log.Println("Src Iface Mask:", p.srcIfaceMask)
+	log.Println("tunnelIP4Dst Mask:", int2ip(p.tunnelIP4DstMask))
+	log.Println("tunnelTEIDMask Mask:", p.tunnelTEIDMask)
+	log.Println("srcIP Mask:", int2ip(p.srcIPMask))
+	log.Println("dstIP Mask:", int2ip(p.dstIPMask))
+	log.Println("srcPort Mask:", p.srcPortMask)
+	log.Println("dstPort Mask:", p.dstPortMask)
+	log.Println("proto Mask:", p.protoMask)
+	log.Println("pdrID:", p.pdrID)
+	log.Println("fseID", p.fseID)
+	log.Println("ctrID:", p.ctrID)
+	log.Println("farID:", p.farID)
+	log.Println("needDecap:", p.needDecap)
+	log.Println("--------------------------------------------")
+}
+
+func (p *pdr) parsPDI(pdiIEs []*ie.IE) error {
+	var ueIP4 net.IP
+
+	for _, pdiIE := range pdiIEs {
+		switch pdiIE.Type {
+		case ie.UEIPAddress:
+			ueIPaddr, err := pdiIE.UEIPAddress()
+			if err != nil {
+				log.Println("Failed to parse UE IP address")
+				continue
+			}
+
+			ueIP4 = ueIPaddr.IPv4Address
+		case ie.SourceInterface:
+			srcIface, err := pdiIE.SourceInterface()
+			if err != nil {
+				log.Println("Failed to parse Source Interface IE!")
+				continue
+			}
+
+			if srcIface == ie.SrcInterfaceCPFunction {
+				log.Println("Source Interface CP Function not supported yet")
+			} else if srcIface == ie.SrcInterfaceAccess {
+				p.srcIface = access
+				p.srcIfaceMask = 0xFF
+			} else if srcIface == ie.SrcInterfaceCore {
+				p.srcIface = core
+				p.srcIfaceMask = 0xFF
+			}
+		case ie.FTEID:
+			fteid, err := pdiIE.FTEID()
+			if err != nil {
+				log.Println("Failed to parse FTEID IE")
+				continue
+			}
+			teid := fteid.TEID
+			tunnelIPv4Address := fteid.IPv4Address
+
+			if teid != 0 {
+				p.tunnelTEID = teid
+				p.tunnelTEIDMask = 0xFFFFFFFF
+				p.tunnelIP4Dst = ip2int(tunnelIPv4Address)
+				p.tunnelIP4DstMask = 0xFFFFFFFF
+				log.Println("TunnelIPv4Address:", tunnelIPv4Address)
+			}
+		case ie.QFI:
+			// Do nothing for the time being
+		}
+	}
+
+	// Needed if SDF filter is bad or absent
+	if len(ueIP4) == 4 {
+		if p.srcIface == core {
+			p.dstIP = ip2int(ueIP4)
+			p.dstIPMask = 0xffffffff // /32
+		} else if p.srcIface == access {
+			p.srcIP = ip2int(ueIP4)
+			p.srcIPMask = 0xffffffff // /32
+		}
+	}
+
+	for _, ie2 := range pdiIEs {
+		switch ie2.Type {
+		case ie.SDFFilter:
+			// Do nothing for the time being
+			sdfFields, err := ie2.SDFFilter()
+			if err != nil {
+				log.Println("Unable to parse SDF filter!")
+				continue
+			}
+
+			flowDesc := sdfFields.FlowDescription
+			if flowDesc == "" {
+				log.Println("Empty SDF filter description!")
+				// TODO: Implement referencing SDF ID
+				continue
+			}
+			log.Println("Flow Description is:", flowDesc)
+
+			var ipf ipFilterRule
+			err = ipf.parseFlowDesc(flowDesc, ueIP4.String())
+			if err != nil {
+				return errBadSDF
+			}
+
+			if ipf.proto != reservedProto {
+				p.proto = ipf.proto
+				p.protoMask = reservedProto
+			}
+
+			if p.srcIface == core {
+				p.dstIP = ip2int(ipf.dst.IPNet.IP)
+				p.dstIPMask = ipMask2int(ipf.dst.IPNet.Mask)
+				p.srcIP = ip2int(ipf.src.IPNet.IP)
+				p.srcIPMask = ipMask2int(ipf.src.IPNet.Mask)
+			} else if p.srcIface == access {
+				p.srcIP = ip2int(ipf.dst.IPNet.IP)
+				p.srcIPMask = ipMask2int(ipf.dst.IPNet.Mask)
+				p.dstIP = ip2int(ipf.src.IPNet.IP)
+				p.dstIPMask = ipMask2int(ipf.src.IPNet.Mask)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (p *pdr) parsePDR(ie1 *ie.IE, seid uint64) error {
+	/* reset outerHeaderRemoval to begin with */
+	outerHeaderRemoval := uint8(0)
+
+	pdrID, err := ie1.PDRID()
+	if err != nil {
+		log.Println("Could not read PDR ID!")
+		return nil
+	}
+
+	precedence, err := ie1.Precedence()
+	if err != nil {
+		log.Println("Could not read Precedence!")
+		return nil
+	}
+
+	pdi, err := ie1.PDI()
+	if err != nil {
+		log.Println("Could not read PDI!")
+		return nil
+	}
+
+	res, err := ie1.OuterHeaderRemovalDescription()
+	if res == 0 && err == nil { // 0 == GTP-U/UDP/IPv4
+		outerHeaderRemoval = 1
+	}
+
+	err = p.parsPDI(pdi)
+	if err != nil && err != errBadSDF {
+		return err
+	}
+
+	farID, err := ie1.FARID()
+	if err != nil {
+		log.Println("Could not read FAR ID!")
+		return nil
+	}
+
+	p.precedence = precedence
+	p.pdrID = uint32(pdrID)
+	p.fseID = uint32(seid) // fseID currently being truncated to uint32 <--- FIXIT/TODO/XXX
+	p.ctrID = 0            // ctrID currently not being set <--- FIXIT/TODO/XXX
+	p.farID = uint8(farID) // farID currently not being set <--- FIXIT/TODO/XXX
+	p.needDecap = outerHeaderRemoval
+
+	return nil
+}
---
+++ pfcpiface/session-far.go
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright(c) 2020 Intel Corporation
+
+package main
+
+import (
+	"errors"
+)
+
+// CreateFAR appends far to existing list of FARs in the session
+func (s *PFCPSession) CreateFAR(f far) {
+	s.fars = append(s.fars, f)
+}
+
+// UpdateFAR updates existing far in the session
+func (s *PFCPSession) UpdateFAR(f far) error {
+	for idx, v := range s.fars {
+		if v.farID == f.farID {
+			s.fars[idx] = f
+			return nil
+		}
+	}
+	return errors.New("FAR not found")
+}
+
+// RemoveFAR removes far from existing list of FARs in the session
+func (s *PFCPSession) RemoveFAR(id uint8) (*far, error) {
+	for idx, v := range s.fars {
+		if v.farID == id {
+			s.fars = append(s.fars[:idx], s.fars[idx+1:]...)
+			return &v, nil
+		}
+	}
+	return nil, errors.New("FAR not found")
+}
