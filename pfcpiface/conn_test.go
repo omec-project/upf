ce1e5a4bd8b72d14560e92a63040053c616cf9d9
pudelkoM <pudelkoM@users.noreply.github.com>
2021-12-01 16:01:53 -0800

    [DO NOT MERGE] Add ie test causing panic

---
+++ pfcpiface/conn_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/wmnsk/go-pfcp/ie"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	buf := []byte{0,0,0,16,0}
+
+	parsedIe, err := ie.Parse(buf)
+	if err != nil {
+		log.Error(err)
+		t.FailNow()
+	}
+	log.Warnln("Type:", parsedIe.Type)
+	log.Warnln("Length:", parsedIe.Length)
+	log.Warnln(parsedIe)
+}
