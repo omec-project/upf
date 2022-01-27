a0a6564d46956842469664c3b82e1ed4468856c8
Emanuele Gallone <emanuele@opennetworking.org>
2022-01-27 16:41:33 +0100

    Add simple FAR parsing test

--- pfcpiface/parse-far_test.go
+++ pfcpiface/parse-far_test.go
@@ -4,9 +4,43 @@
 package main
 
 import (
+	"github.com/omec-project/upf-epc/test/integration"
+	"net"
 	"testing"
 )
 
-func Test(t *testing.T) {
-	t.Fail()
+func TestFARParsing(t *testing.T) {
+
+	f := far{}
+
+	op := create
+	var validFARID uint32 = 1
+	var validFARAction uint8 = ActionForward
+
+	validFAR := integration.NewUplinkFAR(integration.IEMethod(op), validFARID, validFARAction)
+	// using invalid action (0)
+	invalidFAR := integration.NewDownlinkFAR(integration.IEMethod(op), 2, 0, 100, "10.0.10.1")
+
+	mockUpf := &upf{
+		accessIP: net.ParseIP("192.168.0.1"),
+		coreIP:   net.ParseIP("10.0.0.1"),
+	}
+
+	err := f.parseFAR(validFAR, 100, mockUpf, op)
+	if err != nil {
+		t.Errorf("Error parsing FAR: %v", err)
+	}
+
+	if f.farID != validFARID {
+		t.Errorf("Error parsing FAR: FAR ID does not match")
+	}
+
+	if f.applyAction != validFARAction {
+		t.Errorf("Error parsing FAR: FAR action does not match")
+	}
+
+	err = f.parseFAR(invalidFAR, 101, mockUpf, op)
+	if err == nil {
+		t.Errorf("Error parsing FAR: FAR is invalid but no error is returned")
+	}
 }
