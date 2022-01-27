4969aa80d780d0611fda4e85d1b7e254d23805c0
Emanuele Gallone <emanuele@opennetworking.org>
2022-01-27 09:41:38 +0100

    Make draft for testing

---
+++ pfcpiface/mockUpf.go
@@ -0,0 +1,5 @@
+package main
+
+type MockUpf struct {
+	Upf *upf
+}
---
+++ pfcpiface/test_parse-far.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParseFar(t *testing.T) {
+	mock := 1
+
+	t.Fatalf("Mock :%v", mock)
+}
