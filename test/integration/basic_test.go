414ea57182c26e997c3c4276d45abd2502cfbf66
tosinski <tomasz@opennetworking.org>
2022-02-05 19:01:54 +0100

    Prepare test case

--- test/integration/basic_test.go
+++ test/integration/basic_test.go
@@ -4,6 +4,7 @@
 package integration
 
 import (
+	"fmt"
 	p4rtc "github.com/antoninbas/p4runtime-go-client/pkg/client"
 	"github.com/antoninbas/p4runtime-go-client/pkg/util/conversion"
 	p4_v1 "github.com/p4lang/p4runtime/go/p4/v1"
@@ -18,13 +19,24 @@ import (
 	"github.com/wmnsk/go-pfcp/ie"
 )
 
+const (
+	configDefault = "default.json"
+	configUPFBasedIPAllocation = "ue_ip_alloc.json"
+)
+
 var (
 	pfcpClient *pfcpsim.PFCPClient
 )
 
+type testContext struct {
+	UPFBasedUeIPAllocation bool
+}
+
 type testCase struct {
+	ctx      testContext
 	input    *pfcpSessionData
 	expected p4RtValues
+
 	desc     string
 }
 
@@ -32,11 +44,6 @@ func init() {
 	if err := initMockUP4(); err != nil {
 		panic("failed to initialize mock-up4")
 	}
-
-	providers.RunDockerCommand("pfcpiface", "/bin/pfcpiface -config /config.json")
-
-	// wait for PFCP Agent to initialize
-	time.Sleep(time.Second * 3)
 }
 
 func initMockUP4() (err error) {
@@ -84,7 +91,13 @@ func initMockUP4() (err error) {
 	return nil
 }
 
-func setup(t *testing.T) {
+func setup(t *testing.T, pfcpAgentConfig string) {
+	providers.RunDockerCommandAttach("pfcpiface",
+		fmt.Sprintf("/bin/pfcpiface -config /config/%s", pfcpAgentConfig))
+
+	// wait for PFCP Agent to initialize
+	time.Sleep(time.Second * 3)
+
 	pfcpClient = pfcpsim.NewPFCPClient("127.0.0.1")
 	err := pfcpClient.ConnectN4("127.0.0.1")
 	require.NoErrorf(t, err, "failed to connect to UPF")
@@ -94,10 +107,45 @@ func teardown(t *testing.T) {
 	if pfcpClient != nil {
 		pfcpClient.DisconnectN4()
 	}
+
+	// kill pfcpiface process inside container
+	_, _, _, err := providers.RunDockerExecCommand("pfcpiface", "killall -9 pfcpiface")
+	require.NoError(t, err, "failed to kill pfcpiface process")
+}
+
+func TestUPFBasedUeIPAllocation(t *testing.T) {
+	setup(t, configUPFBasedIPAllocation)
+	defer teardown(t)
+
+	tc := testCase{
+		ctx: testContext{
+			UPFBasedUeIPAllocation: true,
+		},
+		input: &pfcpSessionData{
+			nbAddress:    nodeBAddress,
+			ueAddress:    ueAddress,
+			upfN3Address: upfN3Address,
+			sdfFilter:    "permit out ip from any to assigned",
+			ulTEID:       15,
+			dlTEID:       16,
+			sessQFI:      0x09,
+			appQFI:       0x08,
+		},
+		expected: p4RtValues{
+			// no application filtering rule expected
+			appID:        0,
+			tunnelPeerID: 2,
+		},
+		desc: "UPF-based UE IP allocation",
+	}
+
+	t.Run(tc.desc, func(t *testing.T) {
+		testUEAttachDetach(t, &tc)
+	})
 }
 
 func TestBasicPFCPAssociation(t *testing.T) {
-	setup(t)
+	setup(t, configDefault)
 	defer teardown(t)
 
 	err := pfcpClient.SetupAssociation()
@@ -109,7 +157,7 @@ func TestBasicPFCPAssociation(t *testing.T) {
 }
 
 func TestSingleUEAttachAndDetach(t *testing.T) {
-	setup(t)
+	setup(t, configDefault)
 	defer teardown(t)
 
 	// Application filtering test cases
@@ -194,6 +242,10 @@ func TestSingleUEAttachAndDetach(t *testing.T) {
 	}
 }
 
+func fillExpected(tc *testCase) {
+	if 
+}
+
 func testUEAttachDetach(t *testing.T, testcase *testCase) {
 	err := pfcpClient.SetupAssociation()
 	require.NoErrorf(t, err, "failed to setup PFCP association")
@@ -208,14 +260,35 @@ func testUEAttachDetach(t *testing.T, testcase *testCase) {
 			WithFARID(1).
 			AddQERID(4).
 			AddQERID(1).BuildPDR(),
-		session.NewPDRBuilder().MarkAsDownlink().
+	}
+
+	if !testcase.ctx.UPFBasedUeIPAllocation {
+		pdrs = append(pdrs,
+			session.NewPDRBuilder().MarkAsDownlink().
 			WithMethod(session.Create).
 			WithID(2).
 			WithUEAddress(testcase.input.ueAddress).
 			WithSDFFilter(testcase.input.sdfFilter).
 			WithFARID(2).
 			AddQERID(4).
-			AddQERID(2).BuildPDR(),
+			AddQERID(2).BuildPDR())
+	} else {
+		pdrs = append(pdrs,
+			// TODO: should be replaced by builder?
+			ie.NewCreatePDR(
+				ie.NewPDRID(2),
+				ie.NewPrecedence(testcase.input.precedence),
+				ie.NewPDI(
+					ie.NewSourceInterface(ie.SrcInterfaceCore),
+					// indicate UP to allocate UE IP Address
+					ie.NewUEIPAddress(0x10, "", "", 0, 0),
+					ie.NewSDFFilter(testcase.input.sdfFilter, "", "", "", 1),
+				),
+				ie.NewFARID(2),
+				ie.NewQERID(2),
+				ie.NewQERID(4),
+			),
+		)
 	}
 
 	fars := []*ie.IE{
--- test/integration/providers/docker.go
+++ test/integration/providers/docker.go
@@ -5,12 +5,16 @@ package providers
 
 import (
 	"context"
+	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"io"
+	"io/ioutil"
+	"os/exec"
+	"strings"
 )
 
-func RunDockerCommand(container string, cmd string) {
+func RunDockerCommandAttach(container string, cmd string) {
 	inout := make(chan []byte)
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
@@ -43,3 +47,43 @@ func RunDockerCommand(container string, cmd string) {
 	waiter.Conn.Close()
 	waiter.Close()
 }
+
+func RunDockerExecCommand(container string, cmd string) (
+	code int, stdout string, stderr string, err error,
+) {
+	args := make([]string, 0)
+	args = append(args, "exec", "-t", container)
+	if strings.Contains(cmd, "/bin/sh") {
+		// Just split in to "/bin/sh" "-c" and "actual_cmd"
+		// This is useful for passing piped commands in to os/exec interface.
+		args = append(args, strings.SplitN(cmd, " ", 3)...)
+	} else {
+		args = append(args, strings.Fields(cmd)...)
+	}
+	dockerCmd := exec.Command("docker", args...)
+	stdoutPipe, err := dockerCmd.StdoutPipe()
+	if err != nil {
+		return 0, "", "", fmt.Errorf("error when connecting to stdout: %v", err)
+	}
+	stderrPipe, err := dockerCmd.StderrPipe()
+	if err != nil {
+		return 0, "", "", fmt.Errorf("error when connecting to stderr: %v", err)
+	}
+	if err := dockerCmd.Start(); err != nil {
+		return 0, "", "", fmt.Errorf("error when starting command: %v", err)
+	}
+
+	stdoutBytes, _ := ioutil.ReadAll(stdoutPipe)
+	stderrBytes, _ := ioutil.ReadAll(stderrPipe)
+
+	if err := dockerCmd.Wait(); err != nil {
+		if e, ok := err.(*exec.ExitError); ok {
+			return e.ExitCode(), string(stdoutBytes), string(stderrBytes), nil
+		}
+		return 0, "", "", err
+	}
+
+	// command is successful
+	return 0, string(stdoutBytes), string(stderrBytes), nil
+}
+
