f4c610c6201e177ecce1f1719c0dac18139be3cc
pudelkoM <pudelkoM@users.noreply.github.com>
2022-02-09 14:37:13 -0800

    Move config handling out of `main.go`

---
+++ pfcpiface/config_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io/fs"
+	"io/ioutil"
+	"testing"
+)
+
+func mustWriteStringToDisk(s string, path string) {
+	err := ioutil.WriteFile(path, []byte(s), fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestParseJSON(t *testing.T) {
+	t.Run("config is preserved", func(t *testing.T) {
+		s := `{
+			"mode": "dpdk",
+			"log_level": "info",
+			"workers": 1,
+			"max_sessions": 50000,
+			"table_sizes": {
+				"pdrLookup": 50000,
+				"appQERLookup": 200000,
+				"sessionQERLookup": 100000,
+				"farLookup": 150000
+			},
+			"access": {
+				"ifname": "access"
+			},
+			"core": {
+				"ifname": "core"
+			},
+			"measure_upf": true,
+			"measure_flow": true,
+			"enable_notify_bess": true,
+			"notify_sockaddr": "/pod-share/notifycp",
+			"cpiface": {
+				"dnn": "internet",
+				"hostname": "upf",
+				"http_port": "8080"
+			},
+			"n6_bps": 1000000000,
+			"n6_burst_bytes": 12500000,
+			"n3_bps": 1000000000,
+			"n3_burst_bytes": 12500000,
+			"qci_qos_config": [{
+				"qci": 0,
+				"cbs": 50000,
+				"ebs": 50000,
+				"pbs": 50000,
+				"burst_duration_ms": 10,
+				"priority": 7
+			}]
+		}`
+		confPath := t.TempDir() + "/conf.json"
+		mustWriteStringToDisk(s, confPath)
+
+		got, err := ParseJSON(confPath)
+		require.NoError(t, err)
+		assert.Equal(t, "info", got.LogLevel)
+	})
+}
