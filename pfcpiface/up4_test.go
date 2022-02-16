76ecb76ce0754330382d3729ac79aead3c43bab8
Emanuele Gallone <emanuele@opennetworking.org>
2022-02-16 22:00:54 +0100

    Address review comments

--- pfcpiface/up4_test.go
+++ pfcpiface/up4_test.go
@@ -10,43 +10,15 @@ import (
 )
 
 func Test_UP4_initTunnelPeerIDs(t *testing.T) {
-	type args struct {
-		mockUP4 *UP4
-	}
-
-	type want struct {
-		length int
-	}
-
-	tests := []struct {
-		name string
-		args *args
-		want *want
-	}{
-		{
-			name: "init tunnelPeerIDPool",
-			args: &args{
-				mockUP4: &UP4{},
-			},
-			want: &want{
-				length: maxGTPTunnelPeerIDs,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(
-			tt.name, func(t *testing.T) {
-				tt.args.mockUP4.initTunnelPeerIDs()
-				require.Equal(t, tt.want.length, len(tt.args.mockUP4.tunnelPeerIDsPool))
-			},
-		)
-	}
+	up4 := &UP4{}
+	up4.initTunnelPeerIDs()
+	require.Equal(t, maxGTPTunnelPeerIDs, len(up4.tunnelPeerIDsPool))
 }
 
 func Test_UP4_allocateGTPTunnelPeerID(t *testing.T) {
 	type args struct {
 		numAllocate int
-		mockUP4     *UP4
+		up4         *UP4
 	}
 
 	type want struct {
@@ -63,7 +35,7 @@ func Test_UP4_allocateGTPTunnelPeerID(t *testing.T) {
 		{
 			name: "allocate GTPTunnelPeerIDs",
 			args: &args{
-				mockUP4:     &UP4{},
+				up4:         &UP4{},
 				numAllocate: 2,
 			},
 			want: &want{
@@ -75,10 +47,10 @@ func Test_UP4_allocateGTPTunnelPeerID(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				tt.args.mockUP4.initTunnelPeerIDs()
+				tt.args.up4.initTunnelPeerIDs()
 
 				for i := 0; i < tt.args.numAllocate; i++ {
-					got, err := tt.args.mockUP4.allocateGTPTunnelPeerID()
+					got, err := tt.args.up4.allocateGTPTunnelPeerID()
 					if tt.wantErr {
 						require.Error(t, err)
 						return
@@ -87,7 +59,7 @@ func Test_UP4_allocateGTPTunnelPeerID(t *testing.T) {
 					require.Equal(t, tt.want.startID+uint8(i), got)
 				}
 
-				require.Equal(t, tt.want.remainingInPool, len(tt.args.mockUP4.tunnelPeerIDsPool))
+				require.Equal(t, tt.want.remainingInPool, len(tt.args.up4.tunnelPeerIDsPool))
 			},
 		)
 	}
