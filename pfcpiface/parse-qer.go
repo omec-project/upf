830d7fe9be4e9291d0c4ed139f25d828347084a0
Carmelo Cascone <carmelo@opennetworking.org>
2022-02-16 19:11:12 -0800

    Log QER gate status

--- pfcpiface/parse-qer.go
+++ pfcpiface/parse-qer.go
@@ -26,8 +26,10 @@ type qer struct {
 
 func (q qer) String() string {
 	return fmt.Sprintf("QER(id=%v, F-SEID=%v, F-SEID IP=%v, QFI=%v, "+
-		"uplinkMBR=%v, downlinkMBR=%v, uplinkGBR=%v, downlinkGBR=%v)",
-		q.qerID, q.fseID, q.fseidIP, q.qfi, q.ulMbr, q.dlMbr, q.ulGbr, q.dlGbr)
+		"uplinkMBR=%v, downlinkMBR=%v, uplinkGBR=%v, downlinkGBR=%v, "+
+		"uplinkStatus=%v, downlinkStatus=%v)",
+		q.qerID, q.fseID, q.fseidIP, q.qfi, q.ulMbr, q.dlMbr, q.ulGbr, q.dlGbr,
+		q.ulStatus, q.dlStatus)
 }
 
 func (q *qer) parseQER(ie1 *ie.IE, seid uint64) error {
