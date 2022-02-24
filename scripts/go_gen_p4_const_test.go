aa536e9eadac83e34fbfc835e5a85688a5d252be
Emanuele Gallone <emanuele@opennetworking.org>
2022-02-24 15:15:06 +0100

    Add test for constants and table generation

--- scripts/go_gen_p4_const_test.go
+++ scripts/go_gen_p4_const_test.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"github.com/traefik/yaegi/interp"
-	"github.com/traefik/yaegi/stdlib"
 )
 
+const dummy_p4info = "dummy_p4info.txt"
+
 func Test_generate(t *testing.T) {
-	interpreter := interp.New(interp.Options{})
+	p4config := getP4Config(dummy_p4info)
 
-	err := interpreter.Use(stdlib.Symbols)
-	require.NoError(t, err)
+	test := generateConstants(p4config)
 
+	require.True(t, strings.Contains(test, "HdrPreQosPipeSessionsUplinkTeid \t uint32 = 2"))
+	require.True(t, strings.Contains(test, "TablePreQosPipeSessionsDownlink \t uint32 = 34742049"))
+	require.True(t, strings.Contains(test, "ActionParamPreQosPipeSetSessionUplinkSessionMeterIdx \t uint32 = 1"))
+	require.True(t, strings.Contains(test, "DirectCounterAcls \t uint32 = 325583051"))
+	require.True(t, strings.Contains(test, "PacketMetaPacketOut \t uint32 = 75327753"))
+	require.True(t, strings.Contains(test, "MeterPreQosPipeAppMeter \t uint32 = 338231090"))
+}
+
+func Test_generateTables(t *testing.T) {
+	p4config := getP4Config(dummy_p4info)
+
+	test := generateTables(p4config)
+
+	require.True(t, strings.Contains(test, "39015874:\"PreQosPipe.Routing.routes_v4\","))
+	require.True(t, strings.Contains(test, "34778590:\"PreQosPipe.terminations_downlink\","))
 }
