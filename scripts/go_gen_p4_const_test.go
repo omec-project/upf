10c76c398d5c5d5e62766832a67fdca7c6e07be6
Emanuele Gallone <emanuele@opennetworking.org>
2022-02-24 15:53:23 +0100

    Refactor

--- scripts/go_gen_p4_const_test.go
+++ scripts/go_gen_p4_const_test.go
@@ -1,32 +1,75 @@
 package main
 
 import (
+	"strconv"
 	"strings"
 	"testing"
 
+	p4ConfigV1 "github.com/p4lang/p4runtime/go/p4/config/v1"
 	"github.com/stretchr/testify/require"
 )
 
-const dummy_p4info = "dummy_p4info.txt"
+const dummyP4info = "dummy_p4info.txt"
 
 func Test_generate(t *testing.T) {
-	p4config := getP4Config(dummy_p4info)
+	type args struct {
+		p4config *p4ConfigV1.P4Info
+	}
 
-	test := generateConstants(p4config)
+	type want struct {
+		ID   int
+		name string
+	}
 
-	require.True(t, strings.Contains(test, "HdrPreQosPipeSessionsUplinkTeid \t uint32 = 2"))
-	require.True(t, strings.Contains(test, "TablePreQosPipeSessionsDownlink \t uint32 = 34742049"))
-	require.True(t, strings.Contains(test, "ActionParamPreQosPipeSetSessionUplinkSessionMeterIdx \t uint32 = 1"))
-	require.True(t, strings.Contains(test, "DirectCounterAcls \t uint32 = 325583051"))
-	require.True(t, strings.Contains(test, "PacketMetaPacketOut \t uint32 = 75327753"))
-	require.True(t, strings.Contains(test, "MeterPreQosPipeAppMeter \t uint32 = 338231090"))
-}
+	tests := []struct {
+		name    string
+		args    *args
+		want    *want
+		wantErr bool
+	}{
+		{
+			name: "verify table const",
+			args: &args{
+				p4config: getP4Config(dummyP4info),
+			},
+			want: &want{
+				ID:   40931612,
+				name: "TablePreQosPipeMyStation",
+			},
+		},
+		{
+			name: "verify action const",
+			args: &args{
+				p4config: getP4Config(dummyP4info),
+			},
+			want: &want{
+				ID:   23766285,
+				name: "ActionPreQosPipeInitializeMetadata",
+			},
+		},
+		{
+			name: "non existing const",
+			args: &args{
+				p4config: getP4Config(dummyP4info),
+			},
+			want: &want{
+				ID:   111111,
+				name: "test",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := generateConstants(tt.args.p4config)
 
-func Test_generateTables(t *testing.T) {
-	p4config := getP4Config(dummy_p4info)
+			idx := strings.Index(result, tt.want.name)
+			if idx == -1 && tt.wantErr {
+				return
+			}
 
-	test := generateTables(p4config)
-
-	require.True(t, strings.Contains(test, "39015874:\"PreQosPipe.Routing.routes_v4\","))
-	require.True(t, strings.Contains(test, "34778590:\"PreQosPipe.terminations_downlink\","))
+			line := strings.SplitN(result[idx:], "\n", 1)
+			require.True(t, strings.Contains(strings.Join(line, " "), strconv.Itoa(tt.want.ID)))
+		})
+	}
 }
