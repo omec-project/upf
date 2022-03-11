97c64ca31fcdda4b1eacc5a13a6e3878c656a566
pudelkoM <pudelkoM@users.noreply.github.com>
2022-03-11 12:36:40 -0800

    Split verify files for UP4 and BESS

--- test/integration/verify_up4.go
+++ test/integration/verify_up4.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	p4rtc "github.com/antoninbas/p4runtime-go-client/pkg/client"
 	"github.com/antoninbas/p4runtime-go-client/pkg/util/conversion"
-	"github.com/omec-project/upf-epc/pkg/fake_bess"
 	"github.com/omec-project/upf-epc/test/integration/providers"
 	p4_v1 "github.com/p4lang/p4runtime/go/p4/v1"
 	"github.com/stretchr/testify/require"
@@ -418,55 +417,4 @@ func verifyNoP4RuntimeEntries(t *testing.T, expectedValues p4RtValues) {
 		require.Equal(t, 0, len(entries),
 			fmt.Sprintf("%v should not contain any entries", table))
 	}
-}
-
-func verifyBessEntries(t *testing.T, bess *fake_bess.FakeBESS, testdata *pfcpSessionData, expectedValues p4RtValues, ueState UEState) {
-	// Check we have all expected PDRs.
-	pdrs := bess.GetPdrTableEntries()
-	require.Equal(t, len(expectedValues.pdrs), len(pdrs), "found unexpected PDR entries %v", pdrs)
-	for _, expectedPdr := range expectedValues.pdrs {
-		id, err := expectedPdr.PDRID()
-		require.NoError(t, err)
-
-		found := false
-		for _, installedPdr := range pdrs {
-			if installedPdr.PdrID == uint32(id) {
-				found = true
-				break
-			}
-		}
-		require.True(t, found, "missing PDR", "expected PDR with ID %v: %+v, got %+v", id, expectedPdr, pdrs)
-	}
-
-	// Check we have all expected FARs.
-	fars := bess.GetFarTableEntries()
-	require.Equal(t, len(expectedValues.pdrs), len(fars), "found unexpected FAR entries %v", fars)
-	for _, expectedFar := range expectedValues.fars {
-		id, err := expectedFar.FARID()
-		require.NoError(t, err)
-
-		found := false
-		for _, installedFar := range fars {
-			if installedFar.FarID == id {
-				found = true
-				break
-			}
-		}
-		require.True(t, found, "missing FAR", "expected FAR with ID %v: %+v, got %+v", id, expectedFar, fars)
-	}
-
-	// Check we have all expected session and app QERs.
-	qers := append(bess.GetSessionQerTableEntries(), bess.GetAppQerTableEntries()...)
-	require.Equal(t, len(expectedValues.qers)*2 /* up and down link */, len(qers), "found unexpected QER entries %v", qers)
-}
-
-func verifyNoBessRuntimeEntries(t *testing.T, bess *fake_bess.FakeBESS) {
-	pdrs := bess.GetPdrTableEntries()
-	require.Equal(t, 0, len(pdrs), "found unexpected PDR entries: %v", pdrs)
-	fars := bess.GetFarTableEntries()
-	require.Equal(t, 0, len(fars), "found unexpected FAR entries: %v", fars)
-	sessionQers := bess.GetSessionQerTableEntries()
-	require.Equal(t, 0, len(sessionQers), "found unexpected session QER entries: %v", sessionQers)
-	appQers := bess.GetAppQerTableEntries()
-	require.Equal(t, 0, len(appQers), "found unexpected app QER entries: %v", appQers)
 }
