2dd0a1df02692b77153bc6b3d3b54488b26c6d17
gatici <gulsum.atici@canonical.com>
2024-07-17 16:25:37 +0300

    Remove extra logging
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- conf/route_control.py
+++ conf/route_control.py
@@ -616,9 +616,7 @@ class RouteController:
             netlink_message (dict): The netlink message.
         """
         try:
-            logger.info("target: %s", target)
             event = netlink_message.get('event')
-            logger.info("event: %s is received.", event)
         except Exception:
             logger.exception("Error parsing netlink message")
             return
