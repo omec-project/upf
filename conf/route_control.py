f76659b6fda199e06e58d8d86b07f44b2baf4555
gatici <gulsum.atici@canonical.com>
2024-07-17 18:32:50 +0300

    Set rtnl_debug to True
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- conf/route_control.py
+++ conf/route_control.py
@@ -811,7 +811,7 @@ def register_signal_handlers(controller: RouteController) -> None:
 if __name__ == "__main__":
     interface_arg, ip_arg, port_arg = parse_args()
     ipr = IPRoute()
-    ndb = NDB(log="debug")
+    ndb = NDB(rtnl_debug=True)
     bess_controller = BessController(ip_arg, port_arg)
     route_controller = RouteController(
         bess_controller=bess_controller,
