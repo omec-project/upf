b4fe8294cdc314e7a6da188f5a4375e97ec47df3
Marikkannu, Suresh <suresh.marikkannu@intel.com>
2025-08-18 06:55:53 -0700

    Address review comments
    Signed-off-by: Marikkannu, Suresh <suresh.marikkannu@intel.com>

--- pfcpiface/session_qer.go
+++ pfcpiface/session_qer.go
@@ -66,12 +66,12 @@ func findItemIndex(slice []uint32, val uint32) int {
 
 // MarkSessionQer : identify and Mark session QER with flag.
 func (s *PFCPSession) MarkSessionQer(qers []qer) {
-	sessQerIDList := make([]uint32, 0)
-
 	if (len(s.pdrs) < 1) {
-		logger.PfcpLog.Infoln("need atleast 1 PDR, it should not be empty")
+		logger.PfcpLog.Infoln("need at least 1 PDR, it should not be empty")
 		return
 	}
+
+	sessQerIDList := make([]uint32, 0)
 	lastPdrIndex := len(s.pdrs) - 1
 	// create search list with first pdr's qerlist */
 	sessQerIDList = append(sessQerIDList, s.pdrs[lastPdrIndex].qerIDList...)
