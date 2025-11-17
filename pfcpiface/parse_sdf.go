8f220378f86175d2f231f2969689c223346fafe5
LinZiyuu <linziyu0205@163.com>
2025-11-17 10:31:12 +0800

    Fix issue #959: UPF crash on malformed Flow-Description in Session Establishment Request
    * Add boundary checks in parseFlowDesc to prevent array out-of-bounds access
    
    The parseFlowDesc function used to access array elements without checking
    bounds when processing malformed SDF filter flow descriptions, leading to
    a potential DoS vulnerability.
    
    Signed-off-by: LinZiyuu <linziyu0205@163.com>

--- pfcpiface/parse_sdf.go
+++ pfcpiface/parse_sdf.go
@@ -139,6 +139,9 @@ func parseFlowDesc(flowDesc, ueIP string) (*ipFilterRule, error) {
 		switch fields[i] {
 		case "from":
 			i++
+			if i >= len(fields) {
+				return nil, errBadFilterDesc
+			}
 			xform(i)
 
 			err := ipf.src.parseNet(fields[i])
@@ -147,8 +150,12 @@ func parseFlowDesc(flowDesc, ueIP string) (*ipFilterRule, error) {
 				return nil, err
 			}
 
-			if fields[i+1] != "to" {
+			if i+1 < len(fields) && fields[i+1] != "to" {
 				i++
+
+				if i >= len(fields) {
+					return nil, errBadFilterDesc
+				}
 
 				err = ipf.src.parsePort(fields[i])
 				if err != nil {
@@ -158,6 +165,9 @@ func parseFlowDesc(flowDesc, ueIP string) (*ipFilterRule, error) {
 			}
 		case "to":
 			i++
+			if i >= len(fields) {
+				return nil, errBadFilterDesc
+			}
 			xform(i)
 
 			err := ipf.dst.parseNet(fields[i])
