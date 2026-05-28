15c2f2cdc3edfbcba28aebba7af8e16d984ac7f1
Arrobo, Gabriel <gabriel.arrobo@intel.com>
2026-05-27 17:13:06 -0700

    Address Copilot's comments
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- scripts/reset_upf.sh
+++ scripts/reset_upf.sh
@@ -5,15 +5,8 @@
 # Usage: reset_upf.sh
 # This script resets the UPF back to DPDK mode.
 
-MODE=dpdk
-
 ACCESS_PCIE=0000:86:00.0
 CORE_PCIE=0000:88:00.0
-
-ACCESS_IFACE=enp134s0
-CORE_IFACE=enp136s0
-
-SET_IRQ_AFFINITY=~/nic/driver/ice-1.9.11/scripts/set_irq_affinity
 
 sudo dpdk-devbind.py -u $ACCESS_PCIE --force
 sudo dpdk-devbind.py -u $CORE_PCIE --force
