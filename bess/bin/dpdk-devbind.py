885e61efc8211a2ea46aeadf67d7294d40d149d2
bhagathkrishnacdac <bhagath.krishna@cdac.in>
2026-08-30 00:09:22 +0530

    refactor: simplify argument parsing and modularize bind option handling
    Signed-off-by: bhagathkrishnacdac <bhagath.krishna@cdac.in>

--- bess/bin/dpdk-devbind.py
+++ bess/bin/dpdk-devbind.py
@@ -41,7 +41,9 @@ import subprocess
 from os.path import exists, abspath, dirname, basename
 
 # constants for duplicate literals
-UNBIND_OPEN_ERROR = "Error: unbind failed for %s - Cannot open %s"
+UNBIND_OPEN_ERROR = "Error: unbind failed for %s - Cannot open %s"  
+BIND_OPEN_ERROR = "Error: bind failed for %s - Cannot open %s"  
+DRIVER_OVERRIDE_PATH = "/sys/bus/pci/devices/%s/driver_override" 
 USAGE_INFO = "Run '%s --usage' for further information"
 
 # The PCI base class for all devices
@@ -404,7 +406,8 @@ def unbind_one(dev_id, force):
     try:
         f = open(filename, "a")
     except:
-        print(UNBIND_OPEN_ERROR % (dev_id, filename))
+        print("Error: unbind failed for %s - Cannot open %s"
+              % (dev_id, filename))
         sys.exit(1)
     f.write(dev_id)
     f.close()
@@ -508,13 +511,15 @@ def bind_one(dev_id, driver, force):
         try:
             f = open(filename, "w")
         except:
-            print(UNBIND_OPEN_ERROR % (dev_id, filename))
+            print("Error: unbind failed for %s - Cannot open %s"
+                  % (dev_id, filename))
             sys.exit(1)
         try:
             f.write("\00")
             f.close()
         except:
-            print(UNBIND_OPEN_ERROR % (dev_id, filename))
+            print("Error: unbind failed for %s - Cannot open %s"
+                  % (dev_id, filename))
             sys.exit(1)
 
 
@@ -628,14 +633,23 @@ def show_status():
     if status_dev == "mempool" or status_dev == "all":
         show_device_status(mempool_devices, "Mempool")
 
+def _handle_bind_flag(opt, arg):
+    """Internal helper to handle bind/unbind flag logic and prevent duplicates."""
+    global b_flag
+    if b_flag is not None:
+        print("Error - Only one bind or unbind may be specified\n")
+        sys.exit(1)
+
+    if opt in ("-u", "--unbind"):
+        b_flag = "none"
+    else:
+        b_flag = arg
+
 def parse_args():
     '''Parses the command-line arguments given by the user and takes the
     appropriate action for each'''
-    global b_flag
-    global status_flag
-    global status_dev
-    global force_flag
-    global args
+    global b_flag, status_flag, status_dev, force_flag, args
+
     if len(sys.argv) <= 1:
         usage()
         sys.exit(0)
@@ -650,26 +664,17 @@ def parse_args():
         sys.exit(1)
 
     for opt, arg in opts:
-        if opt == "--help" or opt == "--usage":
+        if opt in ("--help", "--usage"):
             usage()
             sys.exit(0)
         if opt == "--status-dev":
-            status_flag = True
-            status_dev = arg
-        if opt == "--status" or opt == "-s":
-            status_flag = True
-            status_dev = "all"
+            status_flag, status_dev = True, arg
+        if opt in ("--status", "-s"):
+            status_flag, status_dev = True, "all"
         if opt == "--force":
             force_flag = True
-        if opt == "-b" or opt == "-u" or opt == "--bind" or opt == "--unbind":
-            if b_flag is not None:
-                print("Error - Only one bind or unbind may be specified\n")
-                sys.exit(1)
-            if opt == "-u" or opt == "--unbind":
-                b_flag = "none"
-            else:
-                b_flag = arg
-
+        if opt in ("-b", "-u", "--bind", "--unbind"):
+            _handle_bind_flag(opt, arg)
 
 def do_arg_actions():
     '''do the actual action requested by the user'''
@@ -681,12 +686,12 @@ def do_arg_actions():
     if b_flag is None and not status_flag:
         print("Error: No action specified for devices."
               "Please give a -b or -u option")
-        print(USAGE_INFO % sys.argv[0])
+        print("Run '%s --usage' for further information" % sys.argv[0])
         sys.exit(1)
 
     if b_flag is not None and len(args) == 0:
         print("Error: No devices specified.")
-        print(USAGE_INFO % sys.argv[0])
+        print("Run '%s --usage' for further information" % sys.argv[0])
         sys.exit(1)
 
     if b_flag == "none" or b_flag == "None":
