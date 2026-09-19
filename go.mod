c709d8341d82722c63154a0161ce3c296ea0aa15
Gabriel Arrobo <gabriel.arrobo@intel.com>
2026-09-18 17:01:46 -0700

    Update dependencies and increase test race count (#1308)
    * Update dependencies and increase test race count
    
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>
    
    * Address review comments
    
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>
    
    * Address review comments
    
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>
    
    ---------
    
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- go.mod
+++ go.mod
@@ -4,11 +4,11 @@ go 1.26.0
 
 require (
 	github.com/libp2p/go-reuseport v0.4.0
-	github.com/omec-project/pfcpsim v1.5.2
+	github.com/omec-project/pfcpsim v1.5.3
 	github.com/prometheus/client_golang v1.24.1
 	github.com/wmnsk/go-pfcp v0.0.24
 	go.uber.org/zap v1.28.0
-	google.golang.org/grpc v1.83.2
+	google.golang.org/grpc v1.84.0
 	google.golang.org/protobuf v1.36.12
 )
 
@@ -23,5 +23,5 @@ require (
 	golang.org/x/net v0.59.0 // indirect
 	golang.org/x/sys v0.48.0 // indirect
 	golang.org/x/text v0.42.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20260908043556-f8649ddbbfe6 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260918162117-cecb64721679 // indirect
 )
--- pfcpiface/telemetry_test.go
+++ pfcpiface/telemetry_test.go
@@ -4,6 +4,8 @@
 package pfcpiface
 
 import (
+	"context"
+	"net"
 	"net/http"
 	"testing"
 
@@ -23,6 +25,48 @@ func restoreReg() {
 	prometheus.DefaultRegisterer = backupGlobalRegistry
 }
 
+// newTestPFCPNode builds a *PFCPNode the way NewPFCPNode does, but on an ephemeral
+// local port instead of the hardcoded production PFCPPort (":8805"). setupProm only
+// reads node.upf, so the node needs no real PFCP listener -- and binding the real port
+// makes the test collide with any other PFCP endpoint already running on the host (and
+// NewPFCPNode calls log.Fatalln on a bind failure, which kills the whole test binary
+// rather than just this test). node.metrics is left nil since setupProm never reads it,
+// avoiding the extra global Prometheus registrations a real metrics service would add.
+func newTestPFCPNode(t *testing.T, u *upf) *PFCPNode {
+	t.Helper()
+
+	var lc net.ListenConfig
+
+	conn, err := lc.ListenPacket(context.Background(), "udp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("failed to open test PFCP socket: %v", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	node := &PFCPNode{
+		ctx:        ctx,
+		cancel:     cancel,
+		PacketConn: conn,
+		done:       make(chan struct{}),
+		upf:        u,
+	}
+
+	t.Cleanup(func() {
+		if err := node.Close(); err != nil {
+			t.Errorf("failed to close node: %v", err)
+		}
+
+		if node.metrics != nil {
+			if err := node.metrics.Stop(); err != nil {
+				t.Errorf("failed to stop metrics: %v", err)
+			}
+		}
+	})
+
+	return node
+}
+
 func Test_setupProm(t *testing.T) {
 	t.Run("can setup prom multiple times with clearProm", func(t *testing.T) {
 		saveReg()
@@ -30,7 +74,7 @@ func Test_setupProm(t *testing.T) {
 
 		// TODO: use actual mocks
 		upf := &upf{}
-		node := NewPFCPNode(upf)
+		node := newTestPFCPNode(t, upf)
 
 		uc, nc, err := setupProm(http.NewServeMux(), upf, node)
 		if err != nil {
@@ -51,7 +95,7 @@ func Test_setupProm(t *testing.T) {
 
 		// TODO: use actual mocks
 		upf := &upf{}
-		node := NewPFCPNode(upf)
+		node := newTestPFCPNode(t, upf)
 
 		_, _, err := setupProm(http.NewServeMux(), upf, node)
 		if err != nil {
